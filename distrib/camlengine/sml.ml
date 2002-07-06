8f3877b38fa32bf9b1e9960a0d8f8fd2c0074435
Richard Bornat <richard@bornat.me.uk>
2002-07-06 17:01:35 +0000

    some useful smlnj functions

---
+++ distrib/camlengine/sml.ml
@@ -0,0 +1,52 @@
+(* $Id$ *)
+
+module type T = 
+sig    
+    val char_explode : string -> char list
+    val char_implode : char list -> string
+    
+    val explode : string -> string list
+    val implode : string list -> string
+    val nj_fold : ('b * 'a -> 'a) -> 'b list -> 'a -> 'a
+    val nj_revfold : ('b * 'a -> 'a) -> 'b list -> 'a -> 'a
+    val null : 'a list -> bool
+    val thrd : ('a * 'b * 'c) -> 'c
+    
+    val (<*>) : ('b -> 'c) -> ('a -> 'b) -> 'a -> 'c (* compose -- can't do without it *)
+
+    val fSome : 'a ->'a option
+end
+
+module M : T =
+  struct
+    (* useful functions for caml *)
+    let (<*>) f g x = f (g x)
+    
+    let rec nj_fold f xs z = match xs with [] -> z | x::xs -> nj_fold f xs (f (x,z))
+    let rec nj_revfold f xs z = match xs with [] -> z | x::xs -> f (x, nj_revfold f xs z)
+    
+    let char_explode s =
+      let len = String.length s in
+      let rec e n = if n=len then [] else String.get s n :: e (n+1) in
+      e 0
+      
+    let explode = List.map (String.make 1) <*> char_explode
+    
+    let implode = String.concat ""
+    
+    (* let char_implode = implode <*> String.make 1 *)
+    let char_implode cs = 
+      let len = List.length cs in
+      let s = String.create len in
+      let rec ii n cs = 
+        match cs with (c::cs) -> (s.[n]<-c; ii (n+1) cs)
+        |             []      -> ()
+      in
+      (ii 0 cs; s) 
+      
+    let thrd (a,b,c) = c
+    
+    let null xs = xs=[]
+    
+    let fSome v = Some v
+end    
