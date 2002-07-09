2162ee8ae8d4448012c43440fae281e70633e6c4
Richard Bornat <richard@bornat.me.uk>
2002-07-09 21:41:02 +0000

    base type for screen drawers

---
+++ distrib/camlengine/screendraw.ml
@@ -0,0 +1,28 @@
+(* $Id$ *)
+
+module type T =
+  sig
+    type tree and 'a hit and displayclass and hitkind and pos and size
+     and box and textbox and layout
+    
+    val clearView : unit -> unit val viewBox : unit -> box
+    val layout : tree -> layout
+    val defaultpos : layout -> pos
+    val rootpos : layout -> pos
+    val postoinclude : box -> layout -> pos
+    val draw : int list option -> pos -> tree -> layout -> unit
+    val print :
+      out_channel -> int list option -> pos -> tree -> layout -> unit
+    val locateHit :
+      pos -> displayclass option -> hitkind -> pos * tree * layout ->
+        int list hit option
+    val refineSelection : bool
+    val notifyselect :
+      (pos * displayclass) option -> (pos * displayclass) list ->
+        pos * tree * layout -> unit
+    val highlight : pos -> displayclass option -> unit
+    val targetbox : int list option -> layout -> textbox option
+    val samelayout : layout * layout -> bool
+  end
+
+
