981ebe799b6201682571f1036e763bc5d09ee552
Richard Bornat <richard@bornat.me.uk>
2002-09-13 08:10:37 +0000

    doesn't do anything  yet

---
+++ distrib/java_GUI/uk/org/jape/JapePanel.java
@@ -0,0 +1,13 @@
+//
+//  JapePanel.java
+//  japeserver
+//
+//  Created by Richard Bornat on Wed Sep 11 2002.
+//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//
+
+import java.util.Vector;
+
+public class JapePanel {
+    private static Vector panels = new Vector();
+}
