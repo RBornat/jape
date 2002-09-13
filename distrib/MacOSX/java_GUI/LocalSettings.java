98d2aeeb9a4caf3904f8cf66f47d87eebb17511f
Richard Bornat <richard@bornat.me.uk>
2002-09-13 09:28:34 +0000

    enable MacOSX menu settings.

---
+++ distrib/MacOSX/java_GUI/LocalSettings.java
@@ -0,0 +1,39 @@
+//
+//  LocalSettings.java
+//  japeserver
+//
+//  Created by Richard Bornat on Fri Sep 13 2002.
+//  Copyleft 2002 Richard Bornat & Bernard Sufrin. Proper GPL text to be inserted
+//
+
+// the MacOSX version of LocalSettings.  All it does is catch the menu actions from
+// the application menu and divert them to japeserver static methods.
+
+import com.apple.mrj.*;
+
+public class LocalSettings implements  MRJAboutHandler,
+                                       MRJQuitHandler,
+                                       MRJPrefsHandler {
+    
+    public static final boolean aboutMenuItemNeeded = false;
+    public static final boolean quitMenuItemNeeded = false;
+    public static final boolean prefsMenuItemNeeded = false;
+    
+    public void handleAbout() {
+        japeserver.handleAbout();
+    }
+
+    public void handleQuit() {
+        japeserver.handleQuit();
+    }
+    
+    public void handlePrefs() {
+        japeserver.handlePrefs();
+    }
+    
+    public LocalSettings() {
+        MRJApplicationUtils.registerAboutHandler(this);
+        MRJApplicationUtils.registerQuitHandler(this);
+        MRJApplicationUtils.registerPrefsHandler(this);
+    }
+}
