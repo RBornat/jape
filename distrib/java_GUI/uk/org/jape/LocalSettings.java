58a7b9f93b3df37164873c6e43c0af174ce6a97b
Richard Bornat <richard@bornat.me.uk>
2002-09-13 09:25:32 +0000

    see comment in file

---
+++ distrib/java_GUI/uk/org/jape/LocalSettings.java
@@ -0,0 +1,20 @@
+//
+//  LocalSettings.java
+//  japeserver
+//
+//  Created by Richard Bornat on Fri Sep 13 2002.
+//  Copyleft 2002 Richard Bornat & Bernard Sufrin. Proper GPL text to be inserted
+//
+
+// this class, which can be replaced by a platform-specific version to aid 
+// look-and-feel localisation (see for example jape/MacOSX/java_japeserver/LocalSettings.java)
+// or I don't know what else.
+
+public class LocalSettings  {
+	
+	// these overridden in MacOSX LocalSettings
+	public static final boolean aboutMenuItemNeeded = true;
+	public static final boolean quitMenuItemNeeded = true;
+	public static final boolean prefsMenuItemNeeded = true;
+
+}
