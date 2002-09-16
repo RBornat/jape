7761c9f3d72ace2e0f6e52caa22c48b2271e3190
Richard Bornat <richard@bornat.me.uk>
2002-09-16 18:27:19 +0000

    super class of ProofWindow, PanelWindow

---
+++ distrib/java_GUI/uk/org/jape/JapeWindow.java
@@ -0,0 +1,55 @@
+//
+//  JapeWindow.java
+//  japeserver
+//
+//  Created by Richard Bornat on Fri Sep 13 2002.
+//  Copyleft 2002 Richard Bornat & Bernard Sufrin. Proper GPL text to be inserted
+//
+
+import java.util.Enumeration;
+import java.awt.Font;
+import java.util.Iterator;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import java.util.Vector;
+
+public class JapeWindow extends JFrame {
+
+    String title;
+    protected static Vector windowv = new Vector();
+    
+    public JapeWindow(String title) {
+        super(title);
+        this.title=title; // ignoring whatever else may happen outside, this is a uid
+        windowv.add(this);
+    }
+    
+    public static JapeWindow findWindow(String title) {
+        int len = windowv.size();
+        for (int i=0; i<len; i++) {
+            JapeWindow w = (JapeWindow)windowv.get(i);
+            if (w.title.equals(title))
+                return w;
+        }
+        return null;
+    }
+    
+    public static Enumeration windows() {
+        return windowv.elements();
+    }
+    
+    private static void setBar(JapeWindow w) {
+        if (w instanceof ProofWindow || 
+            (w instanceof PanelWindow && japeserver.onMacOS && !LocalSettings.aboutMenuItemNeeded)) 
+            // even panel windows get a menu on MacOS X
+            japeserver.menus.setBar(w);
+    }
+    
+    public static void updateMenuBars() {
+        for (Enumeration e = windowv.elements(); e.hasMoreElements(); ) {
+            JapeWindow w = (JapeWindow)e.nextElement();
+            setBar(w);
+        }
+    }
+    
+}
