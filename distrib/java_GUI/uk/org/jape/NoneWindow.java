db344a35cd66d34b8d5324e9769f7ed33fab6636
Richard Bornat <richard@bornat.me.uk>
2002-09-19 08:55:55 +0000

    works, but scroll pane ignores negative coordinates

--- distrib/java_GUI/uk/org/jape/NoneWindow.java
+++ distrib/java_GUI/uk/org/jape/NoneWindow.java
@@ -10,24 +10,63 @@ public class NoneWindow extends JFrame implements ActionListener {
     boolean inAnApplet = true;
     private boolean laidOut = false;
     private JButton b1, b2, b3;
-
+    
+    private class JScrollablePanel extends JPanel implements Scrollable {
+		public Dimension getPreferredScrollableViewportSize() {
+			System.err.println("they asked the panel for its preferred size");
+			return getPreferredSize();
+		}
+	
+		public int getScrollableUnitIncrement(Rectangle visibleRect,
+											  int orientation,
+											  int direction) {
+			return 10; // for now
+		}
+	
+		public int getScrollableBlockIncrement(Rectangle visibleRect,
+											   int orientation,
+											   int direction) {
+			return 100; // for now
+		}
+	
+		public boolean getScrollableTracksViewportWidth() {
+			return false;
+		}
+	
+		public boolean getScrollableTracksViewportHeight() {
+			return false;
+		}
+    }
+    
+    private JScrollablePanel panel;
+	private JScrollPane scrollPane;
+	
     public NoneWindow() {
-        Container contentPane = getContentPane();
-        contentPane.setLayout(null);
+    	panel = new JScrollablePanel();
+        panel.setLayout(null);
 
         b1 = new JButton("one");
-        contentPane.add(b1);
+        b1.addActionListener(this);
+        panel.add(b1);
         b2 = new JButton("two");
-        contentPane.add(b2);
+        b2.addActionListener(this);
+        panel.add(b2);
         b3 = new JButton("three");
         b3.addActionListener(this);
-        contentPane.add(b3);
+        panel.add(b3);
+        
+        scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                                            JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        getContentPane().add(scrollPane);
 
-        Insets insets = contentPane.getInsets();
+        Insets insets = getContentPane().getInsets();
         b1.setBounds(25 + insets.left, 5 + insets.top, 75, 20);
         b2.setBounds(55 + insets.left, 35 + insets.top, 75, 20);
         b3.setBounds(150 + insets.left, 15 + insets.top, 75, 30);
 
+		computeBounds();
+		panel.revalidate();
+		
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 if (inAnApplet) {
@@ -39,13 +78,40 @@ public class NoneWindow extends JFrame implements ActionListener {
         });
     }
     
+    protected void computeBounds() {
+    	Rectangle r = new Rectangle(0,0,0,0);
+    	r.add(b1.getBounds());
+    	r.add(b2.getBounds());
+    	r.add(b3.getBounds());
+    	System.err.print("computeBounds "+r+"+"+panel.getBounds());
+    	panel.setPreferredSize(r.getSize());
+    	panel.setBounds(r.union(panel.getBounds()));
+    	System.err.println(" "+r+" "+scrollPane.getViewport().getViewPosition());
+    	scrollPane.getViewport().setViewPosition(new Point(0,0));
+    	System.err.println(scrollPane.getViewport().getViewPosition());
+    }
+    
     public void actionPerformed(ActionEvent e) {
-    	b3.repaint();
-    	getContentPane().remove(b3);
-    	Rectangle r = b3.getBounds();
-    	b3.setBounds(r.x+50, r.y+50, r.width, r.height);
-    	getContentPane().add(b3);
-    	b3.repaint();
+    	String s = e.getActionCommand();
+   		if (s.equals("three")) {
+			Rectangle r = b3.getBounds();
+			b3.setBounds(r.x+50, r.y+50, r.width, r.height);
+		}
+		else
+		if (s.equals("two")) {
+			Rectangle r = b2.getBounds();
+			b2.setBounds(r.x-50, r.y, r.width, r.height);
+		}
+		else
+		if (s.equals("one")) {
+			Rectangle r = b1.getBounds();
+			b1.setBounds(r.x, r.y-50, r.width, r.height);
+		}
+		else
+			System.err.println("action command "+s);
+		computeBounds();
+    	panel.revalidate();
+    	panel.repaint();
     }
 
     public static void main(String args[]) {
