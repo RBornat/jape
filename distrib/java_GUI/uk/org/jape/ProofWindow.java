1d85f73ad8d88efaf509eb7fcbfb3f6b85110d2c
Richard Bornat <richard@bornat.me.uk>
2002-10-16 16:44:49 +0000

    can now drawstring a little bit

--- distrib/java_GUI/uk/org/jape/ProofWindow.java
+++ distrib/java_GUI/uk/org/jape/ProofWindow.java
@@ -31,48 +31,33 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JScrollPane;
 import java.awt.Point;
+import java.awt.Rectangle;
 
 public class ProofWindow extends JapeWindow {
     int proofnum;
     protected static ProofWindow focussedproof = null;
 
-    protected ProofCanvas canvas; // Sun say use JPanel, but for now ...
-    protected JScrollPane proofpane;
+    protected KPanel proofpanel; 
+    protected JapeScrollPane proofpane;
+
+    protected KPanel focussedpanel;
     
     public ProofWindow(String title, int proofnum) {
         super(title);
         this.proofnum = proofnum;
 
         getContentPane().setLayout(new BorderLayout()); 
-        canvas = new ProofCanvas();
-        canvas.setLayout(null);
-        canvas.setSize(LocalSettings.proofPanelDefaultSize);
-        proofpane = new JScrollPane(canvas, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                                            JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        proofpane = new JapeScrollPane();
 
         getContentPane().add(proofpane, BorderLayout.CENTER);
         setBar(); 
         pack();
         setVisible(true);
         focussedproof = this;
-
-        canvas.setBounds(0,0,200,200); // just to see if it makes a difference, which I bet it does ...
-        System.err.println("canvas bounds are "+canvas.getBounds());
-        
-        TextItem t = new TextItem(canvas, new Point(50, 50), "foobaz", 2);
-        t.selected=true;
-        canvas.add(t);
-        t = new TextItem(canvas, new Point(50, 100), "is best for you", 2);
-        t.selected=true;
-        t.greyed=true;
-        canvas.add(t);
-        t = new TextItem(canvas, new Point(50, 150), "on \u22d6 April \u22d7 Thursdays", 2);
-        canvas.add(t);
-        // canvas.repaint();
-        
     }
     
     public static ProofWindow spawn(String title, int proofnum) throws ProtocolError {
@@ -86,13 +71,87 @@ public class ProofWindow extends JapeWindow {
         // for now, I only deal with one proof window
         return focussedproof;
     }
-    
-    public static void tellDimension() throws ProtocolError {
-        if (focussedproof==null) 
+
+    private static void checkFocussedProof() throws ProtocolError {
+        if (focussedproof==null)
             throw new ProtocolError("no focussed proof");
-        else {
-            Dimension d = focussedproof.getSize();
-            Reply.reply("0 0 "+d.width+" "+d.height);
+    }
+
+    private static void checkFocussedProofPanel() throws ProtocolError {
+        checkFocussedProof();
+        if (focussedproof.proofpanel==null)
+            throw new ProtocolError("no focussed proof panel - clearProofPane missing?");
+    }
+
+    private static void checkFocussedPanel() throws ProtocolError {
+        checkFocussedProof();
+        if (focussedproof.focussedpanel==null)
+            throw new ProtocolError("no focussed panel - drawInPane missing?");
+    }
+        
+    public static Rectangle getViewportBounds() throws ProtocolError {
+        checkFocussedProof();
+        return focussedproof.proofpane.getViewportBounds();
+    }
+
+    private String style;
+    private int linethickness;
+    
+    public static void setProofParams(String style, int linethickness) throws ProtocolError {
+        checkFocussedProof();
+        focussedproof.style = style;
+        focussedproof.linethickness = linethickness;
+    }
+
+    public static void clearProofPane() throws ProtocolError {
+        checkFocussedProof();
+        focussedproof.newProofPanel();
+    }
+
+    private void newProofPanel() {
+        proofpanel = new KPanel();
+        proofpane.add(proofpanel);
+        if (style!=null) {
+            if (style.equals("box"))
+                proofpane.setanchor(proofpane.ANCHOR_SOUTHWEST);
+            else
+            if (style.equals("tree"))
+                proofpane.setanchor(proofpane.ANCHOR_SOUTH);
+        } 
+        proofpane.validate(); proofpane.repaint();
+        focussedpanel = proofpanel;
+    }
+
+    public static void drawInPane(String s) throws ProtocolError {
+        if (s.equals("proof")) {
+            checkFocussedProofPanel();
+            focussedproof.focussedpanel = focussedproof.proofpanel;
         }
+        else
+            throw (new ProtocolError("drawInPane "+s));
+    }
+
+    public static void setGivens(String[] gs) throws ProtocolError {
+        checkFocussedProof();
+        focussedproof.newGivens(gs);
+    }
+
+    private void newGivens(String[] gs) throws ProtocolError {
+        if (gs.length!=0)
+            throw (new ProtocolError("can't (yet) set givens "+gs));
+    }
+
+    public static void clearProvisoView() throws ProtocolError {
+        // do nothing for the time being
+    }
+
+    public static void drawstring(int x, int y, int font, int kind, String annottext, String printtext) throws ProtocolError {
+        checkFocussedPanel();
+        // ignore kind for the moment; ditto annottext
+        JLabel l = new JLabel(printtext);
+        l.setFont(JapeFont.getFont(font));
+        TextDimension td = JapeFont.measure(l, printtext);
+        l.setBounds(x,y-td.ascent,td.width, td.ascent+td.descent);
+        focussedproof.focussedpanel.add(l);
     }
 }
