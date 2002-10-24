72050aa3b12baa8d6bf826851ceb517f74a14c01
Richard Bornat <richard@bornat.me.uk>
2002-10-24 18:05:50 +0000

    lotsa little changes, and I think that text selection / selection now works.

--- distrib/java_GUI/uk/org/jape/ProofCanvas.java
+++ distrib/java_GUI/uk/org/jape/ProofCanvas.java
@@ -28,23 +28,17 @@
 import java.awt.Component;
 import java.awt.event.MouseEvent;
 
-public class ProofCanvas extends JapeCanvas implements SelectionConstants {
-    static final byte BoxStyle = 0,
-                      TreeStyle = 1;
-
-    // bit selectors for selections
-    static final byte HypSel       = 1<<0,
-                      ConcSel      = 1<<1,
-                      ReasonSel    = 1<<2;
+public class ProofCanvas extends JapeCanvas
+                         implements ProofConstants, SelectionConstants {
 
     public ProofCanvas() { super(); }
 
-    protected void declareSelection(SelectableTextItem item, MouseEvent event, byte selkind) {
+    protected void declareSelection(byte eventKind, byte selkind) {
         switch (selkind) {
             case HypSel:
                 // policy at present is that HypSel kills all ReasonSels,
-                // and all other HypSels unless the shift key is down
-                killSelections((byte)(ReasonSel | (event.isShiftDown() ? NoSel : HypSel)));
+                // and all other HypSels unless extended/disjoint
+                killSelections((byte)(ReasonSel | ((eventKind&(ExtendedSelMask|DisjointSelMask))!=0 ? NoSel : HypSel)));
                 break;
             case ConcSel:
                 // ConcSel kills all ReasonSels and all other ConcSels
@@ -57,13 +51,12 @@ public class ProofCanvas extends JapeCanvas implements SelectionConstants {
             default:
                 Alert.abort("ProofCanvas.selectionMade selkind="+selkind);
         }
-        item.select(selkind); 
     }
 
     protected void declareTextSelection(SelectableTextItem item, byte eventKind) {
         switch (eventKind) {
             case TextSelection:
-                // policy is that all text selections are killed, including current selection
+                // policy is that all text selections are killed, including current item
                 killTextSelections(null);
                 break;
             case ExtendedTextSelection:
@@ -78,13 +71,32 @@ public class ProofCanvas extends JapeCanvas implements SelectionConstants {
                 Alert.abort("ProofCanvas.declareTextSelection eventKind="+eventKind);
         }
     }
-    
+
+    // not efficient, not in time order
+    // always ends with a blank line
     public String getTextSelections() {
-        return ""; // for now
+        String s = null;
+        int nc = child.getComponentCount(); // oh dear ...
+        for (int i=0; i<nc; i++) {
+            Component c = child.getComponent(i); // oh dear ...
+            if (c instanceof SelectableTextItem) {
+                SelectableTextItem sti = (SelectableTextItem)c;
+                String s1 = sti.getTextSelectionStrings();
+                if (s1!=null) {
+                    s1 = sti.idX+Reply.stringSep+sti.idY+Reply.stringSep+s1+"\n";
+                    if (s==null)
+                        s=s1;
+                    else
+                        s=s+s1;
+                }
+            }
+        }
+        return s==null ? "" : s;
     }
 
     // these are not yet coming out in time order ...
     // reply always ends with a blank line
+    // not yet efficient
     public String getSelections() {
         String s = null;
         int nc = child.getComponentCount(); // oh dear ...
@@ -97,10 +109,11 @@ public class ProofCanvas extends JapeCanvas implements SelectionConstants {
                     continue;
                 else
                 switch (sti.selectionRect.selkind) {
-                    case HypSel   : selclass = TextItem.HypKind; break;
-                    case ConcSel  : selclass = TextItem.ConcKind; break;
-                    case ReasonSel: selclass = TextItem.ReasonKind; break;
-                    default       : Alert.abort("ProofCanvas.reportSelections selkind="+sti.selectionRect.selkind);
+                    case HypSel   : selclass = HypTextItem; break;
+                    case ConcSel  : selclass = ConcTextItem; break;
+                    case ReasonSel: selclass = ReasonTextItem; break;
+                    default       : Alert.abort("ProofCanvas.reportSelections selkind="+
+                                                sti.selectionRect.selkind);
                                     selclass=0; // shut up compiler
                 }
                 String s1 = sti.idX+" "+sti.idY+" "+selclass+"\n";
