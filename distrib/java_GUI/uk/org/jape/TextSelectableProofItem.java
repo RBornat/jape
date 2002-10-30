6c935c12ff805faed23378b4e1015e30e2c9b3e4
Richard Bornat <richard@bornat.me.uk>
2002-10-30 09:23:47 +0000

    provides blacken, greyen

---
+++ distrib/java_GUI/uk/org/jape/TextSelectableProofItem.java
@@ -0,0 +1,47 @@
+/*
+    $Id$
+    
+    Copyright © 2002 Richard Bornat & Bernard Sufrin
+    
+    richard@bornat.me.uk
+    sufrin@comlab.ox.ac.uk
+    
+    This file is part of japeserver, which is part of jape.
+    
+    Jape is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    Jape is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with jape; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    (or look at http://www.gnu.org).
+
+  */
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class TextSelectableProofItem extends TextSelectableItem {
+    public TextSelectableProofItem(JapeCanvas canvas, int x, int y, byte fontnum,
+                                   String annottext, String printtext) {
+        super(canvas, x, y, fontnum, annottext, printtext);
+    }
+
+    boolean drawGrey = false;
+
+    public void paint(Graphics g) {
+        if (drawGrey) {
+            g.setColor(Color.gray);
+            g.drawChars(printchars, 0, printchars.length, 0, dimension.ascent);
+        }
+        else
+            super.paint(g);
+    }
+}
