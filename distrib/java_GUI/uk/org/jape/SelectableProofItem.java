8ce8e2d1b15f2f2334f2f979d40b99d62064339f
Richard Bornat <richard@bornat.me.uk>
2002-10-30 10:27:20 +0000

    selectionRect is now a little closer

--- distrib/java_GUI/uk/org/jape/SelectableProofItem.java
+++ distrib/java_GUI/uk/org/jape/SelectableProofItem.java
@@ -46,7 +46,7 @@ public abstract class SelectableProofItem extends TextSelectableProofItem
                                String annottext, String printtext) {
         super(canvas,x,y,fontnum,annottext,printtext);
         this.canvas = canvas;
-        selectionRect = new SelectionRect(canvas.getSelectionHalo(), getBounds());
+        selectionRect = new SelectionRect(canvas.linethickness, getBounds());
         canvas.add(selectionRect);
         addMouseInteractionListener(new MouseInteractionAdapter() {
             public void clicked(byte eventKind, MouseEvent e) {
