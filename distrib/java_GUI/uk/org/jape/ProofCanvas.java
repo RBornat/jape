0cb62ad792dd1cf607b1b4fa4a1be1616fc48e7f
Richard Bornat <richard@bornat.me.uk>
2002-10-30 09:27:33 +0000

    notifies SELECT, DESELECT

--- distrib/java_GUI/uk/org/jape/ProofCanvas.java
+++ distrib/java_GUI/uk/org/jape/ProofCanvas.java
@@ -39,7 +39,6 @@ public class ProofCanvas extends JapeCanvas implements ProtocolConstants, Select
             super.add(c, 0);
         else
             super.add(c);
-        c.repaint();
         return c;
     }
 
@@ -48,22 +47,48 @@ public class ProofCanvas extends JapeCanvas implements ProtocolConstants, Select
     // these are not yet coming out in time order ...
     // reply always ends with a blank line
     // not yet efficient
-    public String getSelections() {
+    public String getSelections(String sep) {
         String s = null;
         int nc = child.getComponentCount(); // oh dear ...
         for (int i=0; i<nc; i++) {
-            Component c = child.getComponent(i); // oh dear ...
-            byte selclass;
-            if (c instanceof DisplayItem && c instanceof SelectableItem &&
-                (selclass = protocolSelClass("getSelections", (SelectableItem)c))!=PunctTextItem) {
-                String s1 = ((DisplayItem)c).idX+" "+((DisplayItem)c).idY+" "+selclass+"\n";
+            String s1 =getSelection(child.getComponent(i)); // oh dear ...
+            if (s1!=null) {
                 if (s==null)
                     s=s1;
                 else
-                    s=s+s1;
+                    s=s+sep+s1;
             }
         }
-        return s==null ? "" : s; 
+        return s; 
+    }
+
+    protected String getSelection(Component c) {
+        byte selclass;
+        if (c instanceof DisplayItem && c instanceof SelectableItem &&
+            (selclass = protocolSelClass("getSelection", (SelectableItem)c))!=PunctTextItem) {
+            return ((DisplayItem)c).idX+" "+((DisplayItem)c).idY+" "+selclass;
+        }
+        else
+            return null;
+    }
+    
+    protected void notifySelect(DisplayItem d) {
+        String s = "SELECT "+getSelection(d);
+        int nc = child.getComponentCount(); // oh dear ...
+        for (int i=0; i<nc; i++) {
+            Component c = child.getComponent(i); // oh dear ...
+            if (c!=d) {
+                String s1 = getSelection(c);
+                if (s1!=null)
+                    s = s+" "+s1;
+            }
+        }
+        Reply.send(s);
+    }
+
+    protected void notifyDeselect() {
+        String s = getSelections(" ");
+        Reply.send("DESELECT"+(s==null ? "" : " "+s));
     }
 
     protected byte protocolSelClass(String id, SelectableItem si) {
@@ -80,7 +105,7 @@ public class ProofCanvas extends JapeCanvas implements ProtocolConstants, Select
     
     // not efficient, not in time order
     // always ends with a blank line
-    public String getTextSelections() {
+    public String getTextSelections(String sep) {
         String s = null;
         int nc = child.getComponentCount(); // oh dear ...
         for (int i=0; i<nc; i++) {
@@ -89,15 +114,15 @@ public class ProofCanvas extends JapeCanvas implements ProtocolConstants, Select
                 TextSelectableItem sti = (TextSelectableItem)c;
                 String s1 = sti.getTextSelections();
                 if (s1!=null) {
-                    s1 = sti.idX+Reply.stringSep+sti.idY+Reply.stringSep+s1+"\n";
+                    s1 = sti.idX+Reply.stringSep+sti.idY+Reply.stringSep+s1;
                     if (s==null)
                         s=s1;
                     else
-                        s=s+s1;
+                        s=s+sep+s1;
                 }
             }
         }
-        return s==null ? "" : s;
+        return s;
     }
 
     protected void notifyHit(DisplayItem di) {
@@ -106,5 +131,16 @@ public class ProofCanvas extends JapeCanvas implements ProtocolConstants, Select
                        protocolSelClass("notifyHit",(SelectableItem)di));
         else
             Alert.abort("ProofCanvas.notifyHit di="+di);
+    }
+
+    public SelectableProofItem findXY(int x, int y) {
+        int nc = child.getComponentCount(); // oh dear ...
+        for (int i=0; i<nc; i++) {
+            Component c = child.getComponent(i); // oh dear ...
+            if (c instanceof SelectableProofItem &&
+                ((SelectableProofItem)c).idX==x && ((SelectableProofItem)c).idY==y)
+                return (SelectableProofItem)c;
+        }
+        return null;
     }
 }
