eba4a8f84310530982be0a14bbe37fc1c8667456
Richard Bornat <richard@bornat.me.uk>
2002-11-05 19:32:31 +0000

    curried an argument to measuretext; and cosmetics

--- distrib/camlengine/text.mli
+++ distrib/camlengine/text.mli
@@ -25,23 +25,22 @@ open Box
 type font = Displayfont.displayfont
 
 type textalign = FirstLine | MidBlock | LastLine
-type syllable =
-    Syllable of (font * string)
-  | Gap of int
-  | Linebreak of int
-  | Block of (textalign * syllable list)
+type syllable = Syllable of (font * string)
+	      | Gap of int
+	      | Linebreak of int
+	      | Block of (textalign * syllable list)
 type text = Text of syllable list
 type textlayout = Textlayout of (pos * font * string) list
+
 (* Offset positions are relative to a baseline of (0,0), 
  * so a single syllable will have an offset of (0,0).
  *) 
 
-val string2text : font -> string -> text
-val measuretext :
-  (font * string -> int * int * int) -> textalign -> text ->
-    textsize * textlayout
+val string2text      : font -> string -> text
+val measuretext      : (font -> string -> int * int * int) -> textalign -> text 
+                    -> textsize * textlayout
 val textlayoutOffset : textlayout -> pos -> textlayout
-val textalignstring : textalign -> string
-val syllablestring : syllable -> string
-val textstring : text -> string
+val textalignstring  : textalign -> string
+val syllablestring   : syllable -> string
+val textstring       : text -> string
 val textlayoutstring : textlayout -> string
