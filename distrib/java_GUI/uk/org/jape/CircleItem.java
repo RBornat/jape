fc4d0ca0c326bcadb12b849221d056959ed9b105
Richard Bornat <richard@bornat.me.uk>
2002-11-11 18:39:56 +0000

    a more rational use of Graphics2D

--- distrib/java_GUI/uk/org/jape/CircleItem.java
+++ distrib/java_GUI/uk/org/jape/CircleItem.java
@@ -29,11 +29,13 @@ import java.awt.BasicStroke;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class CircleItem extends Component {
 
     JapeCanvas canvas;
     int radius;
+    protected RenderingHints renderingHints;
     
     public CircleItem(JapeCanvas canvas, int x, int y, int radius) {
         super();
@@ -41,17 +43,17 @@ public class CircleItem extends Component {
         int width = 2*(radius+canvas.linethickness);
         setBounds(x-radius-canvas.linethickness, y-radius-canvas.linethickness, width, width);
         setForeground(Preferences.LineColour);
+        renderingHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                                            RenderingHints.VALUE_ANTIALIAS_ON);
     }
 
-    protected void prepaint(Graphics g) {
+    public void paint(Graphics g) {
         g.setColor(getForeground());
         if (g instanceof Graphics2D) {
             BasicStroke stroke = new BasicStroke((float)canvas.linethickness);
             ((Graphics2D)g).setStroke(stroke);
+            ((Graphics2D)g).addRenderingHints(renderingHints);
         }
-    }
-
-    public void paint(Graphics g) {
-        prepaint(g); g.drawOval(canvas.linethickness, canvas.linethickness, 2*radius, 2*radius);
+        g.drawOval(canvas.linethickness, canvas.linethickness, 2*radius, 2*radius);
     }
 }
