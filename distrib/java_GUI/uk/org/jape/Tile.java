45a3480a9c7bf5c974767fa65873a9564dec6a6d
Richard Bornat <richard@bornat.me.uk>
2002-11-13 18:39:55 +0000

    now re-incorporates stuff to do with dragging.

--- distrib/java_GUI/uk/org/jape/Tile.java
+++ distrib/java_GUI/uk/org/jape/Tile.java
@@ -25,14 +25,39 @@
     
 */
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
+import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceEvent;
+import java.awt.dnd.DragSourceListener;
+
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.SwingUtilities;
+
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
-import java.awt.Graphics;
-import javax.swing.JLabel;
 
-public class Tile extends JLabel implements DebugConstants {
+public class Tile extends JLabel implements DebugConstants,
+                                            DragSourceListener, DragGestureListener {
     final String text;
-    
+    private Container layeredPane;
+    private DragSource dragSource;
+    public static DataFlavor tileFlavor;
+                                                
     static final int spacing = LocalSettings.TileSpacing;
     static final Border padding = BorderFactory.createEmptyBorder(spacing/2,spacing,spacing/2,spacing),
                         raisedbevel = BorderFactory.createRaisedBevelBorder(),
@@ -40,10 +65,107 @@ public class Tile extends JLabel implements DebugConstants {
                         compoundbevel = BorderFactory.createCompoundBorder(raisedbevel, loweredbevel),
                         border = BorderFactory.createCompoundBorder(compoundbevel, padding);
 
-    public Tile(String text) {
+    public Tile(Container layeredPane, final String text) {
         super(text); this.text = text;
-        setFont(JapeFont.getFont(ProtocolConstants.termFontNum));
+        setFont(JapeFont.getFont(ProtocolConstants.TermFontNum));
         setBorder(border);
         setSize(getPreferredSize());
+
+        this.layeredPane = layeredPane;
+        if (tileFlavor==null) {
+            try {
+                tileFlavor = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType+
+                                            "; class="+this.getClass().getName());
+            } catch (ClassNotFoundException e) {
+                Alert.abort("can't create tileFlavor");
+            }
+        }
+
+        dragSource = new DragSource();
+        dragSource.createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_COPY, this);
+
+        MouseInteractionListener mil = new MouseInteractionAdapter() {
+            public void doubleclicked(byte eventKind, MouseEvent e) {
+                Reply.sendCOMMAND("tileact \""+JapeFont.toAscii(text)+"\"");
+            }
+            /*public void pressed(byte eventKind, MouseEvent e) {
+            Tile.this.pressed(e);
+            }
+            public void dragged(byte eventKind, MouseEvent e) {
+                Tile.this.dragged(e);
+            }
+            public void clicked(byte eventKind, MouseEvent e) {
+                Tile.this.released(e);
+            }
+            public void released(byte eventKind, MouseEvent e) {
+                Tile.this.released(e);
+            }*/
+        };
+        addMouseListener(mil);
+        addMouseMotionListener(mil);
+    }
+
+
+    /*protected int startx, starty, lastx, lasty;
+    protected Tile draggedTile;
+
+    protected void pressed(MouseEvent e) { // doesn't matter what keys are pressed
+        if (drag_tracing)
+            System.err.print("mouse pressed on tile "+text+" at "+e.getX()+","+e.getY()+
+                             " insets="+getInsets());
+        startx = lastx = e.getX(); starty = lasty = e.getY();
+        layeredPane.add(draggedTile = new Tile(text), JLayeredPane.DRAG_LAYER);
+        draggedTile.setLocation(SwingUtilities.convertPoint(this, 0, 0, layeredPane));
+        if (drag_tracing)
+            System.err.println("; dragged tile at "+draggedTile.getX()+","+draggedTile.getY());
+        draggedTile.repaint();
+    }
+
+    protected void dragged(MouseEvent e) {
+        if (drag_tracing)
+            System.err.print("mouse dragged to "+e.getX()+","+e.getY());
+        draggedTile.repaint();
+        draggedTile.setLocation(draggedTile.getX()+(e.getX()-lastx),
+                                draggedTile.getY()+(e.getY()-lasty));
+        lastx = e.getX(); lasty = e.getY();
+        if (drag_tracing)
+            System.err.println("; dragged tile now at "+draggedTile.getX()+","+draggedTile.getY());
+        draggedTile.repaint();
+    }
+
+    protected void released(MouseEvent e) {
+        if (drag_tracing)
+            System.err.println("mouse released at "+e.getX()+","+e.getY()+
+                               "; dragged tile at "+draggedTile.getX()+","+draggedTile.getY());
+        layeredPane.remove(draggedTile);
+        layeredPane.repaint();
+    } */
+
+
+    protected class TileTransferable implements Transferable {
+        public Object getTransferData(DataFlavor flavor) {
+            return Tile.this;
+        }
+        public DataFlavor[] getTransferDataFlavors() {
+            return new DataFlavor[]{ tileFlavor };
+        }
+        public boolean isDataFlavorSupported(DataFlavor flavor) {
+            return flavor==tileFlavor;
+        }
+    }
+
+    public void dragGestureRecognized(DragGestureEvent event) {
+        dragSource.startDrag (event, DragSource.DefaultCopyDrop, new TileTransferable(), this);
+    }
+
+    public void dragEnter(DragSourceDragEvent event) { }
+
+    public void dragExit(DragSourceEvent event) { }
+
+    public void dragOver(DragSourceDragEvent event) { }
+
+    public void dropActionChanged(DragSourceDragEvent event) { }
+
+    public void dragDropEnd (DragSourceDropEvent event) {
     }
 }
