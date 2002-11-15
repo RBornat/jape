d72846d599c9a9b64d294c1fe06fe6aa704dddc5
Richard Bornat <richard@bornat.me.uk>
2002-11-15 17:26:04 +0000

    now we save disproved things too ...

--- distrib/camlengine/proofstore.ml
+++ distrib/camlengine/proofstore.ml
@@ -1,5 +1,5 @@
 (*
-	$Id$
+    $Id$
 
     This file is part of the jape proof engine, which is part of jape.
 
@@ -44,7 +44,7 @@ let freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
     saveproofs =
 
   let proofs : (name, (bool * prooftree * name list * bool * (seq * model) option)) mapping ref =
-	ref empty
+    ref empty
 
   and allsaved = ref true
 
@@ -57,23 +57,23 @@ let freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
   and clearproofs () = proofs := empty; allsaved := true
 
   and proofnamed name =
-	match thinginfo name, (!proofs <:> name) with
-	  Some (Theorem (_, provisos, seq), _), Some (v, tree, _, disproved, disproof) ->
-		Some (v, tree, provisos, [], disproved, disproof)
-	| Some (Rule ((_, provisos, givens, seq), _), _),
-	  Some (v, tree, _, disproved, disproof) ->
-		Some (v, tree, provisos, givens, disproved, disproof)
-	| _ -> None
+    match thinginfo name, (!proofs <:> name) with
+      Some (Theorem (_, provisos, seq), _), Some (v, tree, _, disproved, disproof) ->
+        Some (v, tree, provisos, [], disproved, disproof)
+    | Some (Rule ((_, provisos, givens, seq), _), _),
+      Some (v, tree, _, disproved, disproof) ->
+        Some (v, tree, provisos, givens, disproved, disproof)
+    | _ -> None
 
   and proof_depends name =
-	match (!proofs <:> name) with
-	  Some (true, _, children, _, _) -> Some children
-	| _ -> None
+    match (!proofs <:> name) with
+      Some (_, _, children, _, _) -> Some children
+    | _ -> None
 
   and proof_children name =
-	match proof_depends name with
-	  Some ns -> ns
-	| None -> []
+    match proof_depends name with
+      Some ns -> ns
+    | None -> []
   
   (* when we prove something, we may prove a different sequent from the one that was 
    * proposed, and a different set of provisos.  We shall check, and update the 
@@ -89,58 +89,58 @@ let freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
   (* and I REALLY REALLY MUST extend this to redefinition of Rules ... *)
 
   and addproof alert query name proved proof givens cxt disproved disproofopt =
-	let cxt = rewritecxt cxt in
-	let seq = rewriteseq cxt (sequent proof) in
-	let provisos = provisos cxt in
-	let ds = depends proof in
-	let rec children n = if n = name then ds else proof_children n in
-	(* don't bother with Rule/Theorem distinction: proof_depends is just as quick *)
-	(* does derived rules stuff matter here? *)
-	let rec putitin name thing givens place =
-	  addthing (name, thing, place);
-	  proofs :=
-		(!proofs ++ (name |-> (proved, proof, ds, disproved, disproofopt)));
-	  allsaved := false;
-	  true
-	in
-	let rec storedprovisos vps =
-	  let rec unvisproviso b vp = b, provisoactual vp in
-	  (unvisproviso true <* (provisovisible <| vps))
-	in
-	let rec doit () =
-	  match givens, thingnamed name with
-		[], Some (Theorem (params, _, _), place) ->
-		  putitin name (Theorem (params, storedprovisos provisos, seq)) []
-			place
-	  | _ :: _, Some (Theorem (params, _, _), place) ->
-		  alert
-			["replacing theorem "; parseablenamestring name;
-			 " with derived rule"];
-		  putitin name
-			(Rule ((params, storedprovisos provisos, givens, seq), false))
-			givens place
-	  | _, Some (Rule ((params, _, _, _), _), place) ->
-		  putitin name
-			(Rule ((params, storedprovisos provisos, givens, seq), false))
-			givens place
-	  | _ ->
-		  raise
-			(Catastrophe_ ["can't find theorem statement in addproof"])
-	in
-	if not proved then doit ()
-	else
-	  (* if it's not proved we don't really care ... *)
-	  match toposort [name] children with
-		_, [] -> doit ()
-	  | _, cycles ->
-		  (* no cycles *)
-		  alert
-			["can't add proof of "; parseablenamestring name;
-			 " because it introduces circularity: ";
-			 liststring
-			   (fun bs -> liststring parseablenamestring " uses " bs)
-			   "; and " cycles];
-		  false
+    let cxt = rewritecxt cxt in
+    let seq = rewriteseq cxt (sequent proof) in
+    let provisos = provisos cxt in
+    let ds = depends proof in
+    let rec children n = if n = name then ds else proof_children n in
+    (* don't bother with Rule/Theorem distinction: proof_depends is just as quick *)
+    (* does derived rules stuff matter here? *)
+    let rec putitin name thing givens place =
+      addthing (name, thing, place);
+      proofs :=
+        (!proofs ++ (name |-> (proved, proof, ds, disproved, disproofopt)));
+      allsaved := false;
+      true
+    in
+    let rec storedprovisos vps =
+      let rec unvisproviso b vp = b, provisoactual vp in
+      (unvisproviso true <* (provisovisible <| vps))
+    in
+    let rec doit () =
+      match givens, thingnamed name with
+        [], Some (Theorem (params, _, _), place) ->
+          putitin name (Theorem (params, storedprovisos provisos, seq)) []
+            place
+      | _ :: _, Some (Theorem (params, _, _), place) ->
+          alert
+            ["replacing theorem "; parseablenamestring name;
+             " with derived rule"];
+          putitin name
+            (Rule ((params, storedprovisos provisos, givens, seq), false))
+            givens place
+      | _, Some (Rule ((params, _, _, _), _), place) ->
+          putitin name
+            (Rule ((params, storedprovisos provisos, givens, seq), false))
+            givens place
+      | _ ->
+          raise
+            (Catastrophe_ ["can't find theorem statement in addproof"])
+    in
+    if not proved then doit ()
+    else
+      (* if it's not proved we don't really care ... *)
+      match toposort [name] children with
+        _, [] -> doit ()
+      | _, cycles ->
+          (* no cycles *)
+          alert
+            ["can't add proof of "; parseablenamestring name;
+             " because it introduces circularity: ";
+             liststring
+               (fun bs -> liststring parseablenamestring " uses " bs)
+               "; and " cycles];
+          false
 
   and saved () = !allsaved
 
@@ -152,19 +152,19 @@ let freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
   let rec saveable () = not (isempty !proofs)
 
   and proved n =
-	match proofnamed n with
-	  Some (proved,_, _, _, _,_) -> proved
-	| _ -> false
+    match proofnamed n with
+      Some (proved,_, _, _, _,_) -> proved
+    | _ -> false
 
   and disproved n =
-	match proofnamed n with
-	  Some (_, _, _, _,disproved,_) -> disproved
-	| _ -> false
+    match proofnamed n with
+      Some (_, _, _, _,disproved,_) -> disproved
+    | _ -> false
 
   and provedordisproved n =
-	match proofnamed n with
-	  Some (proved, _, _, _,disproved,_) -> Some (proved,disproved)
-	| _ -> None
+    match proofnamed n with
+      Some (proved, _, _, _,disproved,_) -> Some (proved,disproved)
+    | _ -> None
   in
 
   let inproofstore = opt2bool <.> proofnamed in
@@ -172,73 +172,69 @@ let freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
   let menu2word _ = "MENU" in
 
   let panel2word p =
-	match getpanelkind p with
-	  Some TacticPanelkind     -> "TACTICPANEL"
-	| Some ConjecturePanelkind -> "CONJECTUREPANEL"
-	| Some GivenPanelkind      -> 
-		raise (Catastrophe_ ["proof in GIVENPANEL"])
-	| None -> 
-	   raise (Catastrophe_ ["proof in unknown panel "; namestring p])
+    match getpanelkind p with
+      Some TacticPanelkind     -> "TACTICPANEL"
+    | Some ConjecturePanelkind -> "CONJECTUREPANEL"
+    | Some GivenPanelkind      -> 
+        raise (Catastrophe_ ["proof in GIVENPANEL"])
+    | None -> 
+       raise (Catastrophe_ ["proof in unknown panel "; namestring p])
   in
 
   let rec saveproof stream name stage tree provisos givens disproof =
-	let rec badthing () =
-	  raise
-		(Catastrophe_
-		   ["No stored conjecture/derived rule called ";
-			parseablenamestring name])
-	in
-	let place =
-	  match thingnamed name with
-		Some (th, place) ->
-		  begin match th with
-			Theorem _ -> place
-		  | Rule (_, false) -> place
-		  | _ -> badthing ()
-		  end
-	  | _ -> badthing ()
-	in
-	let rec doit ss =
-	  let params =
-		match thingnamed name with
-		  Some (Theorem (params, _, _), _) -> params
-		| _ -> []
-	  in
-	  let body =
-		catelim_prooftree2tactic tree provisos givens
-		  (catelim_modelstring disproof ss)
-	  in
-	  proofstage2word stage :: " " :: parseablenamestring name ::
-		(if null params then body
-		 else
-		   "\n(" ::
-			 catelim_liststring catelim_paraparamstring ", " params
-			   (")" :: body))
-	in
-	List.iter (output_string stream)
-	  (match place with
-		 InMenu m ->
-		   menu2word m :: " " :: parseablenamestring m :: "\n" ::
-			 doit ["END\n"]
-	   | InPanel p ->
-		   panel2word p :: " " :: parseablenamestring p :: "\n" ::
-			 doit ["END\n"]
-	   | InLimbo -> doit [])
+    let rec badthing () =
+      raise (Catastrophe_ ["No stored conjecture/derived rule called ";
+                           parseablenamestring name])
+    in
+    let place =
+      match thingnamed name with
+        Some (th, place) ->
+          begin match th with
+            Theorem _       -> place
+          | Rule (_, false) -> place
+          | _               -> badthing ()
+          end
+      | _ -> badthing ()
+    in
+    let rec doit ss =
+      let params =
+        match thingnamed name with
+          Some (Theorem (params, _, _), _) -> params
+        | _                                -> []
+      in
+      let body =
+        catelim_prooftree2tactic tree provisos givens
+          (catelim_modelstring disproof ss)
+      in
+      proofstage2word stage :: " " :: parseablenamestring name ::
+        (if null params then body
+         else
+           "\n(" ::
+             catelim_liststring catelim_paraparamstring ", " params
+               (")" :: body))
+    in
+    List.iter (output_string stream)
+      (match place with
+         InMenu m ->
+           menu2word m :: " " :: parseablenamestring m :: "\n" ::
+             doit ["END\n"]
+       | InPanel p ->
+           panel2word p :: " " :: parseablenamestring p :: "\n" ::
+             doit ["END\n"]
+       | InLimbo -> doit [])
 
   and saveproofs stream =
-	let rec show n =
-	  match proofnamed n with
-		Some (proved, tree, provisos, givens, disproved, disproof) ->
-		  saveproof stream n (if proved then Proved else Disproved) tree (* WRONG!!! *)
-			((snd <* (fst <| provisos))) givens
-			disproof
-	  | _ -> ()
-	in
-	let names = proved <| thingnames () in
-	let (sortednames, _) =
-	  toposort names (fun n -> proved <| proof_children n)
-	in
-	revapp show sortednames; allsaved := true
+    let rec show n =
+      match proofnamed n with
+        Some (proved, tree, provisos, givens, disproved, disproof) ->
+          saveproof stream n Complete tree ((snd <* (fst <| provisos))) givens disproof
+      | _ -> ()
+    in
+    let names = (opt2bool <.> proofnamed) <| thingnames () in
+    let (sortednames, _) = toposort names (fun n -> proved <| proof_children n)
+    in
+    revapp show sortednames; allsaved := true
+    
   in
   freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
   proof_children, addproof, saved, proofnames, provedthings, saveable,
@@ -246,32 +242,32 @@ let freezesaved, thawsaved, clearproofs, proofnamed, proof_depends,
   saveproofs
 
 let rec thingswithproofs triv =
-	 (fun n ->
-		match thingnamed n with
-		  None -> false
-		| Some (Tactic _, _) -> false
-		| Some (Macro _, _) -> false
-		| Some (Rule (_, ax), _) -> not ax && (triv || proved n)
-		| Some (Theorem _, _) -> triv || proved n) <|
-	 thingnames ()
+     (fun n ->
+        match thingnamed n with
+          None -> false
+        | Some (Tactic _, _) -> false
+        | Some (Macro _, _) -> false
+        | Some (Rule (_, ax), _) -> not ax && (triv || proved n)
+        | Some (Theorem _, _) -> triv || proved n) <|
+     thingnames ()
 (* why is this different from thingswithproofs??? *)
 
 
 let rec namedthingswithproofs triv ts =
-	(fun n ->
-		match thingnamed n with
-		  None -> false
-		| Some (Tactic _, _) -> true
-		| Some (Macro _, _) -> true
-		| Some (Rule (_, ax), _) -> (ax || triv) || proved n
-		| Some (Theorem _, _) -> triv || proved n) <|
-	 ts
+    (fun n ->
+        match thingnamed n with
+          None -> false
+        | Some (Tactic _, _) -> true
+        | Some (Macro _, _) -> true
+        | Some (Rule (_, ax), _) -> (ax || triv) || proved n
+        | Some (Theorem _, _) -> triv || proved n) <|
+     ts
 
 let thmLacksProof = not <.> proved
 
 let rec needsProof a1 a2 =
   match a1, a2 with
-	name, Rule (_, ax) -> not ax || not (proved name)
+    name, Rule (_, ax) -> not ax || not (proved name)
   | name, Tactic _ -> false
   | name, Macro _ -> false
   | name, Theorem _ -> not (proved name)
