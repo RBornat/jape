4d4a2c83332cfe86c437d9689f2224142304bfae
Richard Bornat <richard@bornat.me.uk>
2002-11-22 16:53:24 +0000

    slightly better ask/askOKCancel interface

--- distrib/java_GUI/uk/org/jape/Alert.java
+++ distrib/java_GUI/uk/org/jape/Alert.java
@@ -34,18 +34,18 @@ import java.util.Vector;
 
 public class Alert {
     // oh the ceaseless dance of interface conversions ..
-    public static final int Plain = JOptionPane.PLAIN_MESSAGE;
-    public static final int Info = JOptionPane.INFORMATION_MESSAGE;
-    public static final int Warning = JOptionPane.WARNING_MESSAGE;
-    public static final int Error = JOptionPane.ERROR_MESSAGE;
+    public static final int Plain    = JOptionPane.PLAIN_MESSAGE;
+    public static final int Info     = JOptionPane.INFORMATION_MESSAGE;
+    public static final int Warning  = JOptionPane.WARNING_MESSAGE;
+    public static final int Error    = JOptionPane.ERROR_MESSAGE;
     public static final int Question = JOptionPane.QUESTION_MESSAGE;
     
     private static int messagekind(int severity) throws ProtocolError {
         switch (severity) {
-          case 0: return Info;
-          case 1: return Warning;
-          case 2: return Error;
-          case 3: return Question;
+          case 0 : return Info;
+          case 1 : return Warning;
+          case 2 : return Error;
+          case 3 : return Question;
           default: throw new ProtocolError(severity+" should be message severity (0:info, 1:warning, 2:error, 3: question)");
         }
     }
@@ -85,7 +85,7 @@ public class Alert {
     }
 
     public static void showAlert(int messagekind, Object message) {
-        showAlert(null,messagekind,message);
+        showAlert(null, messagekind, message);
     }
 
     static String quit="Quit", cont="Continue";
@@ -106,11 +106,26 @@ public class Alert {
     }
 
     // this doesn't deal with fonts yet ... I think we have to make a Component (sigh)
-    // and I haven't worked out what to do with defaultbutton ...
-    public static int ask(String[] buttons, int severity, String message, int defaultbutton)
-        throws ProtocolError{
-        return JOptionPane.showOptionDialog(null, makeMessage(message), null, 0, messagekind(severity),
+    private static int query(Component parent, String[] buttons, int messageKind,
+                             String message, int defaultbutton) {
+        return JOptionPane.showOptionDialog(parent, makeMessage(message), null, 0, messageKind,
                                             null, buttons, buttons[defaultbutton]);
+    }
+    
+    public static int ask(String[] buttons, int severity, String message, int defaultbutton)
+                                 throws ProtocolError {
+        return query(null, buttons, messagekind(severity), message, defaultbutton);
+    }
+
+    public static final int OK     = 0,
+                            Cancel = 1;
+
+    public static int askOKCancel(Component parent, String message) {
+        return query(parent, new String[] { "OK", "Cancel"}, Question, message, 0);
+    }
+
+    public static int askOKCancel(String message) {
+        return askOKCancel(null, message);
     }
 
     public static int askDangerously(String message, String doit, String dont) {
