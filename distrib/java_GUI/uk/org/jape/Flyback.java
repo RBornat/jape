2fb2a7b940ae85a42c27cdb24d296b4b7cdbc9d3
Richard Bornat <richard@bornat.me.uk>
2002-11-27 12:40:08 +0000

    what it says

---
+++ distrib/java_GUI/uk/org/jape/Flyback.java
@@ -0,0 +1,77 @@
+/* 
+    $Id$
+
+    Copyright © 2002 Richard Bornat & Bernard Sufrin
+     
+        richard@bornat.me.uk
+        sufrin@comlab.ox.ac.uk
+
+    This file is part of japeserver, which is part of jape.
+
+    Jape is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Jape is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with jape; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    (or look at http://www.gnu.org).
+    
+*/
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Point;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+public class Flyback implements ActionListener, MiscellaneousConstants {
+    Container pane;
+    Component c;
+    Timer timer;
+
+    int lastx, lasty, destx, desty;
+    
+    public Flyback(Container pane, Component c, Point start, Point dest) {
+        this.pane = pane; this.c = c;
+        this.lastx = start.x; this.lasty = start.y;
+        this.destx = dest.x; this.desty = dest.y;
+        
+        timer = new Timer(1000/FlybackFramesPerSecond, this);
+        timer.setInitialDelay(0);
+        timer.setCoalesce(true);
+
+        timer.start();
+    }
+
+    private int delta(int last, int dest) {
+        if (last<=dest)
+            return Math.min(dest-last, FlybackDelta);
+        else
+            return Math.max(dest-last, -FlybackDelta);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (lastx==destx && lasty==desty) {
+            timer.stop();
+            pane.remove(c);
+            pane.repaint();
+        }
+        else {
+            int nextx = lastx+delta(lastx, destx), nexty = lasty+delta(lasty, desty);
+            c.repaint();
+            c.setLocation(nextx, nexty);
+            c.repaint();
+            lastx = nextx; lasty = nexty;
+        }
+    }
+}
