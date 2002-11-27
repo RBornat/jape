b4292ffa252519a16f6f68282d4b7fb03b35fcca
Richard Bornat <richard@bornat.me.uk>
2002-11-27 18:07:35 +0000

    ends no longer final (but they are private)

--- distrib/java_GUI/uk/org/jape/LineItem.java
+++ distrib/java_GUI/uk/org/jape/LineItem.java
@@ -33,7 +33,7 @@ import java.awt.Graphics2D;
 public class LineItem extends Component implements DebugConstants {
 
     protected final JapeCanvas canvas;
-    protected final int x1, y1, x2, y2;
+    private int x1, y1, x2, y2;
 
     public LineItem(JapeCanvas canvas, int x1, int y1, int x2, int y2) {
         super();
