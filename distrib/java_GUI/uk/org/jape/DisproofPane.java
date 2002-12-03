bef57b16172a6f542a0208c42a18986ef7b3fecf
Richard Bornat <richard@bornat.me.uk>
2002-12-03 14:41:40 +0000

    after much effort, a trash can

--- distrib/java_GUI/uk/org/jape/DisproofPane.java
+++ distrib/java_GUI/uk/org/jape/DisproofPane.java
@@ -30,7 +30,11 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.LayoutManager;
+import java.awt.Toolkit;
+
+import java.awt.image.ImageObserver;
 
 import javax.swing.JFrame;
 
@@ -46,24 +50,31 @@ public class DisproofPane extends Container implements DebugConstants {
     final Container tileCanvas;
 
     JFrame window; // for the draggers and droppers
+
+    private TrashCan trashCan;
     
     public DisproofPane(JFrame window, int linethickness) {
         super();
         this.window = window; this.linethickness = linethickness;
         
         setLayout(new DisproofPaneLayout());
+
         setBackground(Color.white);
+        
         worldPane = new AnchoredScrollPane();
         add(worldPane);
+
         worldCanvas = new WorldCanvas(worldPane.getViewport(), window, true, linethickness);
         worldPane.add(worldCanvas);
         worldPane.setAnchor(AnchoredScrollPane.ANCHOR_SOUTH);
+
         tileCanvas = new Container() {
             public Dimension getPreferredSize() { return getSize(); }
             public Dimension getMinimumSize() { return getSize(); }
         };
         tileCanvas.setLayout(null);
         add(tileCanvas);
+
         seqView = new Container() {
             public void validate() { }
             public Dimension getPreferredSize() { return getSize(); }
@@ -71,10 +82,14 @@ public class DisproofPane extends Container implements DebugConstants {
         };
         add(seqView);
         seqView.setLayout(null);
+
         seqCanvas = new DisproofCanvas(seqView, false) {
             public float getAlignmentX() { return CENTER_ALIGNMENT; }
         };
         seqView.add(seqCanvas);
+
+        trashCan = new TrashCan();
+        add(trashCan);
     }
 
     public void setSequentBox(int width, int ascent, int descent) {
@@ -130,6 +145,8 @@ public class DisproofPane extends Container implements DebugConstants {
         if (tileLayoutPending)
             getLayout().layoutContainer(this); // in case tiles have changed size
     }
+
+    private boolean layout_set = false;
     
     protected class DisproofPaneLayout implements LayoutManager {
 
@@ -198,6 +215,7 @@ public class DisproofPane extends Container implements DebugConstants {
          */
         
         public void layoutContainer(Container pane) {
+            layout_set = true;
             Dimension paneSize = pane.getSize();
             Dimension seqSize = seqView.getSize();
             doTileLayout();
@@ -208,21 +226,161 @@ public class DisproofPane extends Container implements DebugConstants {
                 seqtop = bottom-seqSize.height,
                 tileleft = right-tileCanvasSize.width,
                 worldbottom = seqtop-gap(),
+                tilebottom = worldbottom-worldPane.scrollbarthickness,
                 worldright = tileleft-gap();
             
             tileCanvas.setBounds
-                (tileleft, worldbottom-worldPane.scrollbarthickness-tileCanvasSize.height,
+                (tileleft, tilebottom-tileCanvasSize.height,
                  tileCanvasSize.width, tileCanvasSize.height);
+
+            trashCan.setLocation(tileleft, tilebottom+LocalSettings.TileSpacing);
+            if (!trashCan.scaled())
+                trashCan.notifyHeight(bottom-tilebottom);
+
             worldPane.setBounds(0, 0, worldright, worldbottom);
+
             // centre the sequent on the world pane viewport, otherwise the world looks off-centre
             seqView.setLocation(Math.max(0, worldright-worldPane.scrollbarthickness-seqSize.width)/2,
                                 seqtop);
 
             if (disprooflayout_tracing) {
-                System.err.println(pane); japeserver.showContainer(pane, null); 
+                System.err.println("disproof layout "+pane); japeserver.showContainer(pane, null); 
             }
             
             pane.repaint();
+        }
+    }
+
+    private static Image normal_image, selected_image, unavailable_image,
+                         normal_scaled, selected_scaled, unavailable_scaled;
+
+    protected class TrashCan extends Component {
+        private Image current_image;
+        private float scaleFactor;
+        
+        public TrashCan() {
+            if (normal_image==null) {
+                Toolkit tk = Toolkit.getDefaultToolkit();
+                normal_image = tk.createImage("trashcan.jpg");
+                selected_image = tk.createImage("trashcan_selected.jpg");
+                current_image = null;
+                // trigger imageUpdate
+                if (image_tracing)
+                    System.err.println("dimension request (normal_image)");
+                normal_image.getWidth(this); normal_image.getHeight(this);
+            }
+            else
+                current_image = normal_scaled;
+
+            if (current_image!=null) {
+                // possible concurrency nasty: see comment in TrashCan
+                if (image_tracing)
+                    System.err.println("dimension request (current_image)");
+                int width = current_image.getWidth(this), height = current_image.getHeight(this);
+                setSize(width, height);
+            }
+            else
+                setSize(-1, -1);
+        }
+
+        public boolean scaled() { return current_image!=null; }
+
+        public void paint(Graphics g) {
+            if (current_image!=null) {
+                if (image_tracing)
+                    System.err.println("painting "+getSize());
+                g.drawImage(current_image, 0, 0, this);
+            }
+        }
+
+        public void notifyHeight(int height) {
+            if (image_tracing)
+                System.err.println("notifyHeight("+height+")");
+            if (current_image==null) {
+                if (image_tracing)
+                    System.err.println("dimension request (normal_image) in notifyHeight");
+                int imageheight = normal_image.getHeight(this),
+                    imagewidth = normal_image.getWidth(this);
+                if (imageheight!=-1 && imagewidth!=-1) {
+                    float scale = ((float)height)/((float)imageheight);
+                    current_image = normal_scaled =
+                        normal_image.getScaledInstance((int)(((float)imagewidth)*scale),
+                                                       (int)(((float)imageheight)*scale),
+                                                       Image.SCALE_SMOOTH);
+                    if (image_tracing)
+                        System.err.println("image to be "+
+                                           (((float)imagewidth)*scale)+" x "+
+                                           (((float)imageheight)*scale));
+                    // there's a nasty bit of concurrency here ... be careful.
+                    // I have to getWidth and getHeight and then believe the local answers;
+                    // if either is -1 then I rely on imageUpdate;
+                    // if neither is -1 then I believe them.
+                    // (Well maybe there isn't, but it it's better to be sure.)
+                    if (image_tracing)
+                        System.err.println("dimension request (current_image) in notifyHeight");
+                    int scaledwidth = current_image.getWidth(this),
+                        scaledheight = current_image.getHeight(this);
+                    if (scaledwidth!=-1 && scaledheight!=-1) {
+                        setSize(scaledwidth, scaledheight); repaint();
+                        if (image_tracing) {
+                            System.err.println("immediate repaint "+getBounds());
+                            japeserver.showContainer(DisproofPane.this, null);
+                        }
+                    }
+                }
+                else
+                if (image_tracing)
+                    System.err.println("not yet: "+imageheight+"x"+imagewidth);
+            }
+            else {
+                if (image_tracing)
+                    System.err.println("we are already set");
+            }
+        }
+
+        // This method should return true if further updates are needed
+        // or false if the required information has been acquired
+        private boolean MORE_PLEASE = true, THATS_ENOUGH = false;
+        
+        public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+            if (image_tracing)
+                System.err.println("imageUpdate("+img+",0x"+Integer.toHexString(infoflags)+
+                                   ","+x+","+y+","+width+","+height+")"+
+                                   "; normal_image="+normal_image+
+                                   "; normal_scaled="+normal_scaled+
+                                   "; current_image="+current_image+
+                                   "; WIDTH=0x"+Integer.toHexString(ImageObserver.WIDTH)+
+                                   "; HEIGHT=0x"+Integer.toHexString(ImageObserver.HEIGHT)+
+                                   "; ALLBITS=0x"+Integer.toHexString(ImageObserver.ALLBITS));
+            if ((infoflags & (ImageObserver.WIDTH | ImageObserver.HEIGHT)) ==
+                (ImageObserver.WIDTH | ImageObserver.HEIGHT)) {
+                if (img==normal_image) {
+                    if (image_tracing)
+                        System.err.println("it's the first time");
+                    if (DisproofPane.this.layout_set) {
+                        DisproofPane.this.getLayout().layoutContainer(DisproofPane.this);
+                        if (image_tracing)
+                            System.err.println("triggered a layout");
+                    }
+                    return THATS_ENOUGH;
+                }
+                else {
+                    setSize(width, height); repaint();
+                    if (image_tracing) {
+                        System.err.println("triggered first repaint "+getBounds());
+                        japeserver.showContainer(DisproofPane.this, null);
+                    }
+                    return MORE_PLEASE;
+                }
+            }
+            else
+            if ((infoflags & ImageObserver.ALLBITS) == ImageObserver.ALLBITS) {
+                if (image_tracing)
+                    System.err.println("triggered second repaint "+getBounds());
+                repaint(); return THATS_ENOUGH;
+            }
+            else
+                return MORE_PLEASE; // you get all sorts of intermediate information
         }
     }
 }
