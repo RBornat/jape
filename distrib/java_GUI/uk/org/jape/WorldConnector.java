a621d389b098b2f70a73336c1d1391ac5cda55c2
Richard Bornat <richard@bornat.me.uk>
2002-12-04 16:10:57 +0000

    accepts world drops

--- distrib/java_GUI/uk/org/jape/WorldConnector.java
+++ distrib/java_GUI/uk/org/jape/WorldConnector.java
@@ -25,13 +25,71 @@
     
 */
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
-public class WorldConnector extends LineItem {
+public class WorldConnector extends LineItem implements SelectionConstants, WorldTarget {
     public final WorldItem from, to;
+    public WorldCanvas canvas;
     public WorldConnector(WorldCanvas canvas, JFrame window, WorldItem from, WorldItem to) {
         super(canvas, from.idX, -from.idY, to.idX, -to.idY);
+        this.canvas = canvas;
         this.from = from; this.to = to;
         from.registerFrom(this); to.registerTo(this);
+    }
+
+    /* ****************************** world as drag target ****************************** */
+
+    private static final int wobble = 3;
+    
+    public boolean contains(int x, int y) {
+        if (-wobble<=x && x<getWidth()+wobble && -wobble<=y && y<getHeight()+wobble) {
+            if (x0==x1)
+                return x-x0<=wobble; // vertical
+            else
+            if (y0==y1)
+                return y-y0<=wobble; // horizontal
+            else {
+                float a = y1-y0, b = x0-x1, c = a*x0+b*y0;
+                float dx = x-(c-b*y)/a, dy = y-(c-a*x)/b;
+                return dx<=wobble || dy<=wobble || dx*dx*dy*dy/(dx*dx+dy*dy)<=wobble*wobble;
+            }
+        }
+        else
+            return false;
+    }
+
+    private boolean draghighlight;
+    private Color previousColour;
+    
+    public boolean dragEnter(Object o) {
+        if (o instanceof WorldItem) {
+            previousColour = getForeground();
+            setForeground(Preferences.SelectionColour);
+            canvas.imageRepaint(); repaint();
+            draghighlight = true;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void dragExit() {
+       setForeground(previousColour);
+        canvas.imageRepaint(); repaint();
+        draghighlight = false;
+    }
+
+    /* ****************************** world as drop target ****************************** */
+
+    public void drop(byte dragKind, WorldItem w, int x, int y) {
+        if (draghighlight) {
+            System.err.println((dragKind==MoveWorldDrag ? "moveworldtoline" : "addworldtoline")+
+                              " "+w.idX+" "+w.idY+" "+from+" "+to);
+            dragExit();
+        }
+        else
+            Alert.abort("world drop on non-accepting line");
     }
 }
