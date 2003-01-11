088595869734b486eaaf0d2e40fca3d5316f2b4c
Richard Bornat <richard@bornat.me.uk>
2003-01-11 17:09:28 +0000

    what it says

---
+++ distrib/java_GUI/uk/org/jape/DebugVars.java
@@ -0,0 +1,11 @@
+//
+//  DebugVars.java
+//  japeserver
+//
+//  Created by Richard Bornat on Sat Jan 11 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+public class DebugVars {
+
+}
