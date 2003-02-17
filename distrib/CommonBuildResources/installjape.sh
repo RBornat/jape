d0eb6de553be94a711ac589674a67bdeb0555981
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2003-02-17 18:24:30 +0000

    Installation script works with the new Jape Make script works with the new architecture

--- distrib/CommonBuildResources/installjape.sh
+++ distrib/CommonBuildResources/installjape.sh
@@ -13,13 +13,12 @@ fi
 cat <<ENDSCRIPT>jape
         JAPEHOME=$JAPEHOME 
         export JAPEHOME
-        JAPESERVER=$JAPEHOME/japeserver 
-        export JAPESERVER
-        $JAPEHOME/jape.engine "\$*"
+        exec java -jar \$JAPEHOME/Jape.jar -engine \$JAPEHOME/jape_engine \$*
 ENDSCRIPT
-echo  exec $JAVABIN -jar $JAPEHOME/japeserver.jar > japeserver
-chmod +x jape japeserver jape.engine
+chmod +x jape jape_engine
 echo "Jape is now installed in $JAPEHOME"
 echo "You can make symbolic or other links to the executable $JAPEHOME/jape"
+
+
 
 
