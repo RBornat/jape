fceee01aadbd28d769567497d1dd1d0fc06ab0c0
Richard Bornat <richard@bornat.me.uk>
2003-02-19 21:59:25 +0000

    cosmetic

--- distrib/java_GUI/uk/org/jape/JapeMenu.java
+++ distrib/java_GUI/uk/org/jape/JapeMenu.java
@@ -25,12 +25,13 @@
     
 */
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
