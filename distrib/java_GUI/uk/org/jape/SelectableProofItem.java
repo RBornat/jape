6c5db80361459a458767fc1610c8daa23937921c
Richard Bornat <richard@bornat.me.uk>
2003-10-21 18:41:08 +0000

    at the heart of the new selection mechanisms, heaven help us

--- distrib/java_GUI/uk/org/jape/SelectableProofItem.java
+++ distrib/java_GUI/uk/org/jape/SelectableProofItem.java
@@ -32,137 +32,53 @@ import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.Rectangle;
 
-public abstract class SelectableProofItem extends    TextSelectableProofItem
-                                          implements SelectableItem,
-                                                     ProtocolConstants {
+public  class SelectableProofItem extends TextSelectableItem
+                               implements ProtocolConstants {
+    private final boolean ambiguous;
+    private byte selectionKind;
 
-    protected SelectionRect selectionRect;
-
-    protected final ProofCanvas canvas;
-
-    public SelectableProofItem(ProofCanvas canvas, int x, int y, byte fontnum,
-                               String annottext) {
+    public SelectableProofItem(ProofCanvas canvas, int x, int y, byte fontnum, String annottext,
+                               byte selectionKind, boolean ambiguous) {
         super(canvas,x,y,fontnum,annottext);
-        this.canvas = canvas;
-        selectionRect = new SelectionRect(canvas.getSurroundGap(), getBounds());
-        canvas.add(selectionRect);
-        addJapeMouseListener(new JapeMouseTextAdapter() {
-            public void clicked(byte eventKind, MouseEvent e) {
-                SelectableProofItem.this.canvas.claimFocus();
-                SelectableProofItem.this.clicked(eventKind, e);
-            }
-            public void doubleclicked(byte eventKind, MouseEvent e) {
-                SelectableProofItem.this.doubleclicked(eventKind, e);
-            }
-        });
+        this.selectionKind = selectionKind; // ReasonSel, HypSel, ConcSel
+        this.ambiguous = ambiguous;
+        addSelectionIndicator(
+            canvas.proofStyle==BoxStyle ? new DirectedFormulaSelection(this) :
+                                          new RectSelection(this));
     }
 
-    protected abstract void clicked(byte eventKind, MouseEvent e);
+    public void setSelectionKind(byte selectionKind) {
+        if (ambiguous ? (selectionKind==HypSel || selectionKind==ConcSel) :
+                        selectionKind==this.selectionKind)
+            this.selectionKind = selectionKind;
+        else
+            Alert.abort("SelectableProofItem.setSelectionKind("+selectionKind+")"+
+                        "; ambiguous="+ambiguous+"; selectionKind="+this.selectionKind);
+    }
 
-    protected void doubleclicked(byte eventKind, MouseEvent e) {
-        canvas.notifyHit(this);
+    public byte getSelectionKind() {
+        return selectionKind;
+    }
+
+    public boolean getAmbiguous() {
+        return ambiguous;
     }
     
-    protected class SelectionRect extends RectItem {
-        public byte selkind;
-
-        SelectionRect(int halo, Rectangle bounds) {
-            super(SelectableProofItem.this.canvas,
-                  bounds.x-halo, bounds.y-halo, bounds.width+2*halo, bounds.height+2*halo);
-            setForeground(Preferences.SelectionColour);
-        }
-
-        protected void paintDotted(Graphics g, int y) {
-            int dashlength = 3*this.canvas.linethickness;
-            for (int i=0; i<right; i+=dashlength) {
-                if ((i/3)%2==0) g.drawLine(i,y, Math.min(right, i+dashlength-1),y);
+    // single click can change perception of an ambiguous formula
+    public void selectionclicked(byte eventKind, MouseEvent e) {
+        if (ambiguous) {
+            byte newSelectionKind = e.getY()<getHeight()/2 ? ConcSel : HypSel;
+            if (newSelectionKind!=selectionKind) {
+                setSelected(false);
+                setSelectionKind(newSelectionKind);
             }
         }
-
-        protected void paintHooks(Graphics g, int y) {
-            int hooklength = Math.min(4*this.canvas.linethickness, getWidth()/6);
-            if (hooklength>this.canvas.linethickness) {
-                int lefthook = hooklength-1, righthook = right-hooklength+1;
-                g.drawLine(left,y, lefthook,y);
-                g.drawLine(righthook,y, right,y);
-            }
-        }
-
-        public void paint(Graphics g) {
-            /*  At present we have two selection styles.  Reasons, and all formulae in tree style,
-            are selected by surrounding them with a box.  In box style hyps get a selection
-            open at the bottom, concs get a selection open at the top.  Ambigs behave differently
-            when clicked in different places: near the top you get a conc-style selection, near
-            the bottom a hyp-style selection, but in each case the closed end of the box is a
-            dotted line.
-            */
-
-            if (paint_tracing)
-                Logger.log.println("painting proof item at "+getX()+","+getY());
-            
-            if (selkind!=NoSel) {
-                prepaint(g);
-
-                switch (selkind) {
-                    case ReasonSel:
-                        super.paint(g); break;
-
-                    case HypSel:
-                        if (SelectableProofItem.this.canvas.proofStyle==BoxStyle) {
-                            paintSides(g); paintHorizEdge(g, top); paintHooks(g, bottom);
-                        }
-                        else
-                            super.paint(g);
-                        break;
-
-                    case HypSel | AmbigSel:
-                        paintSides(g); paintDotted(g, top); paintHooks(g, bottom); break;
-
-                    case ConcSel:
-                        if (SelectableProofItem.this.canvas.proofStyle==BoxStyle) {
-                            paintSides(g); paintHorizEdge(g, bottom); paintHooks(g, top);
-                        }
-                        else
-                            super.paint(g);
-                        break;
-
-                    case ConcSel | AmbigSel:
-                        paintSides(g); paintDotted(g, bottom); paintHooks(g, top); break;
-
-                    default:
-                        Alert.abort("SelectableProofItem.SelectionRect selkind="+selkind);
-                }
-            }
-
-        }
-
-        public void setSelkind(byte selkind) {
-            this.selkind = selkind; repaint();
-        }
-        
-        public byte getSelkind() {
-            return (byte)(selkind&~AmbigSel);
-        }
+        super.selectionclicked(eventKind, e);
     }
 
-    public byte getSelkind() {
-        return selectionRect.getSelkind();
-    }
-
-    public boolean selkindOverlaps(byte selmask) {
-        return (getSelkind()&selmask)!=0;
-    }
-
-    public void select(byte selkind) { selectionRect.setSelkind(selkind); }
-
-    public void deselect() { selectionRect.setSelkind(NoSel); }
-
-    public void blacken() {
-        setForeground(Preferences.TextColour); repaint();
-    }
-
-    public void greyen() {
-        deselect(); deTextSelect();
-        setForeground(Preferences.GreyTextColour); repaint();
+    // you can't double-click an ambiguous unselected item
+    public void selectiondoubleclicked(byte eventKind, MouseEvent e) {
+        if (!ambiguous || getSelected())
+            super.selectiondoubleclicked(eventKind, e);
     }
 }
