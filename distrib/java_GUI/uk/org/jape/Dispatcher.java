a80d94e2254c8937879abf0981b1754df492457c
Richard Bornat <richard@bornat.me.uk>
2004-05-08 09:01:40 +0000

    interning; DRAWMEASUREDTEXT; errors are errors again

--- distrib/java_GUI/uk/org/jape/Dispatcher.java
+++ distrib/java_GUI/uk/org/jape/Dispatcher.java
@@ -62,11 +62,11 @@ public class Dispatcher extends Thread implements DebugConstants {
 		    }
 		
 		    if (cmd.length!=0) {
-			String p = cmd[0];
+			String p = cmd[0].intern();
 			int len = cmd.length;
 			
 		    // GET means client is listening
-			if (p.equals("GET")&&len==1) {
+			if (p=="GET"&&len==1) {
 			    if (Reply.openchannel()) {
 				JapeWindow.ensureMenusAvailable();
 				ProofWindow.makeReady();
@@ -75,90 +75,107 @@ public class Dispatcher extends Thread implements DebugConstants {
 			else
 
 		    // string passing happens a lot, so put it early
-			if (p.equals("STRINGSIZE")&&len==3)
+			if (p=="STRINGSIZE"&&len==3)
 			    Reply.reply(JapeFont.measure(cmd[2],
 					    JapeFont.checkInterfaceFontnum(toByte(cmd[1]))));
 			else
-			if (p.equals("STRINGSIZE")&&len==2) // this can happen ... ask a stupid question
+			if (p=="STRINGSIZE"&&len==2) // this can happen ... ask a stupid question
 			    Reply.reply(JapeFont.measure("", 
 					      JapeFont.checkInterfaceFontnum(toByte(cmd[1]))));
 			else
-			if (p.equals("DRAWSTRING")&&len==6)
-			    ProofWindow.getFocussedWindow().drawstring(toInt(cmd[1]), toInt(cmd[2]),	// x, y
-						   toByte(cmd[3]), toByte(cmd[4]),  // font, kind
-						   cmd[5]);			    // annottext
+			if (p=="DRAWSTRING"&&len==6)
+			    ProofWindow.getFocussedWindow().drawstring(
+				toInt(cmd[1]), toInt(cmd[2]),       // x, y
+				toByte(cmd[3]), toByte(cmd[4]),     // font, kind
+				cmd[5]);			    // annottext
 			else
-			if (p.equals("DRAWRECT")&&len==5)
+			if (p=="DRAWMEASUREDTEXT"&&len==5)
+			    ProofWindow.getFocussedWindow().startMeasuredText(
+				toInt(cmd[1]), toInt(cmd[2]),       // x, y
+				toByte(cmd[3]), toInt(cmd[4]));      // kind, length
+			else
+			if (p=="DRAWMT"&&len==5)
+			    ProofWindow.getFocussedWindow().continueMeasuredText(
+				 toInt(cmd[1]), toInt(cmd[2]),      // x, y
+				 toByte(cmd[3]), cmd[4]);	    // font, annottext
+			else
+			if (p=="DRAWMEASUREDTEXTEND"&&len==1)
+			    ProofWindow.getFocussedWindow().endMeasuredText();
+			else
+			if (p=="DRAWRECT"&&len==5)
 			    ProofWindow.getFocussedWindow().drawRect(toInt(cmd[1]), toInt(cmd[2]), // x, y
 						 toInt(cmd[3]), toInt(cmd[4])); // w, h
 			else
-			if (p.equals("DRAWLINE")&&len==5)
+			if (p=="DRAWLINE"&&len==5)
 			    ProofWindow.getFocussedWindow().drawLine(toInt(cmd[1]), toInt(cmd[2]), // x, y
 						 toInt(cmd[3]), toInt(cmd[4])); // w, h
 			else
-			if (p.equals("BLACKEN")&&len==3)
+			if (p=="BLACKEN"&&len==3)
 			    ProofWindow.getFocussedWindow().blacken(toInt(cmd[1]), toInt(cmd[2]));
 			else
-			if (p.equals("GREYEN")&&len==3)
+			if (p=="GREYEN"&&len==3)
 			    ProofWindow.getFocussedWindow().greyen(toInt(cmd[1]), toInt(cmd[2]));
 			else
-			if (p.equals("HIGHLIGHT")&&len==4)
+			if (p=="HIGHLIGHT"&&len==4)
 			    ProofWindow.getFocussedWindow().highlight(toInt(cmd[1]), toInt(cmd[2]), toByte(cmd[3]));
 			else
-			if (p.equals("UNHIGHLIGHT")&&len==3)
+			if (p=="UNHIGHLIGHT"&&len==3)
 			    ProofWindow.getFocussedWindow().unhighlight(toInt(cmd[1]), toInt(cmd[2]));
 			else
 			    
 		    // FONTINFO not very often
-			if (p.equals("FONTINFO")&&len==2)
+			if (p=="FONTINFO"&&len==2)
 			    Reply.reply(JapeFont.getFontMetrics(JapeFont.checkInterfaceFontnum(toByte(cmd[1]))));
 			else
-			if (p.equals("FONTNAMES")&&len==1)
+			if (p=="FONTNAMES"&&len==1)
 			    Reply.reply(JapeFont.getFontNames(Reply.stringSep));
 			else
 
 		    // ASK is for alerts
-			if (p.equals("ASKSTART")&&len==1)
+			if (p=="ASKSTART"&&len==1)
 			    list.removeAllElements();
 			else
-			if (p.equals("ASKBUTTON")&&len==2)
+			if (p=="ASKBUTTON"&&len==2)
 			    list.add(cmd[1]);
 			else
-			if (p.equals("ASKNOW")&&len==4)
+			if (p=="ASKNOW"&&len==4)
 			    Reply.reply(Alert.ask(((String[])list.toArray(new String[list.size()])),
 						  toInt(cmd[1]), cmd[2], toInt(cmd[3])));
 			else
-			if (p.equals("ASKDANGEROUSLY")&&len==4)
+			if (p=="ASKDANGEROUSLY"&&len==4)
 			    Reply.reply(Alert.askDangerously(cmd[1], cmd[2], cmd[3]));
 			else
-			if (p.equals("CLEARCHOICES")&&len==1)
+			if (p=="CLEARCHOICES"&&len==1)
 			    ChoiceDialog.clearChoices();
 			else
-			if (p.equals("SETCHOICE")&&len==3)
+			if (p=="SETCHOICE"&&len==3)
 			    ChoiceDialog.setChoice(cmd[1], toInt(cmd[2]));
 			else
-			if (p.equals("MAKECHOICE")&&len==2)
+			if (p=="MAKECHOICE"&&len==2)
 			    Reply.reply(ChoiceDialog.makeChoice(cmd[1]));
+			else
+			if (p=="SETALERT"&&len==2)
+			    Alert.showErrorAlert(cmd[1]);
 			else
 		    
 		    // file choosing
-			if (p.equals("READFILENAME")&&len==3)
+			if (p=="READFILENAME"&&len==3)
 			    Reply.reply(FileChooser.newOpenDialog(cmd[1], cmd[2]));
 			else
-			if (p.equals("WRITEFILENAME")&&len==3)
+			if (p=="WRITEFILENAME"&&len==3)
 			    Reply.reply(FileChooser.newSaveDialog(cmd[1], cmd[2]));
 			else
 			    
 		    // font setting
-			if (p.equals("SETFONTS")&&len==2) {
+			if (p=="SETFONTS"&&len==2) {
 			    Alert.showAlert("The FONTS directive is out of date.\n\n"+
 					    "Please delete it from your source files");
 			}
 			else
 			
 		    // INVISCHARS are the way we describe syntactic structure
-			if(p.equals("SETINVISCHARS")&&len==9)
-			    TextSelectableItem.setinvischars(
+			if(p=="SETINVISCHARS"&&len==9)
+			    AnnotatedTextComponent.setinvischars(
 				toChar(cmd[1]),toChar(cmd[2]),
 				toChar(cmd[3]),toChar(cmd[4]),
 				toChar(cmd[5]),toChar(cmd[6]),
@@ -166,39 +183,39 @@ public class Dispatcher extends Thread implements DebugConstants {
 			else
 		    
 		    // MENU commands
-			if (p.equals("NEWMENU")&&len==3)
+			if (p=="NEWMENU"&&len==3)
 			    JapeMenu.newMenu(toBool(cmd[1]), cmd[2]);
 			else
-			if (p.equals("MENUITEM")&&len==5)
+			if (p=="MENUITEM"&&len==5)
 			    JapeMenu.addItem(cmd[1], cmd[2], cmd[3], cmd[4]);
 			else
-			if (p.equals("MAKEMENUSVISIBLE")&&len==1) {
+			if (p=="MAKEMENUSVISIBLE"&&len==1) {
 			    JapeMenu.makeMenusVisible();
 			    PanelWindowData.makePanelsVisible();
 			}
 			else
-			if (p.equals("MENUSEP")&&len==2)
+			if (p=="MENUSEP"&&len==2)
 			    JapeMenu.addSeparator(cmd[1]);
 			else
-			if (p.equals("ENABLEMENUITEM")&&len==4) {
+			if (p=="ENABLEMENUITEM"&&len==4) {
 			    // showcommand("", cmd);
 			    // see comment in japeserver.mli
 			    JapeMenu.enableItem(false, cmd[1], cmd[2], toBool(cmd[3]));
 			}
 			else
-			if (p.equals("MENURADIOBUTTON")&&len==1)
+			if (p=="MENURADIOBUTTON"&&len==1)
 			    list.removeAllElements();
 			else
-			if (p.equals("MENURADIOBUTTONPART")&&len==3)
+			if (p=="MENURADIOBUTTONPART"&&len==3)
 			    list.add(new String[] { cmd[1], cmd[2] });
 			else
-			if (p.equals("MENURADIOBUTTONEND")&&len==2)
+			if (p=="MENURADIOBUTTONEND"&&len==2)
 			    JapeMenu.addRadioButtonGroup(cmd[1], (String[][])list.toArray(new String[list.size()][]));
 			else
-			if (p.equals("MENUCHECKBOX")&&len==4)
+			if (p=="MENUCHECKBOX"&&len==4)
 			    JapeMenu.addCheckBox(cmd[1], cmd[2], cmd[3]);
 			else
-			if (p.equals("TICKMENUITEM")&&len==4) {
+			if (p=="TICKMENUITEM"&&len==4) {
 			    // showcommand("",cmd);
 			    // see comment in japeserver.mli
 			    JapeMenu.tickItem(false, cmd[1], cmd[2], toBool(cmd[3]));
@@ -206,19 +223,19 @@ public class Dispatcher extends Thread implements DebugConstants {
 			else
 		    
 		    // PANEL commands
-			if (p.equals("NEWPANEL")&&len==3)
+			if (p=="NEWPANEL"&&len==3)
 			    PanelWindowData.spawn(cmd[1], toInt(cmd[2]));
 			else
-			if (p.equals("PANELENTRY")&&len==4)
+			if (p=="PANELENTRY"&&len==4)
 			    PanelWindowData.addEntry(cmd[1], cmd[2], cmd[3]);
 			else
-			if (p.equals("PANELBUTTON")&&len==3) {
+			if (p=="PANELBUTTON"&&len==3) {
 			    list.removeAllElements();
 			    list.add(cmd[1]);
 			    list.add(cmd[2]);
 			}
 			else
-			if (p.equals("PANELBUTTONINSERT")&&len==3) {
+			if (p=="PANELBUTTONINSERT"&&len==3) {
 			    switch (toInt(cmd[1])) {
 			      case 0: list.add(new PanelWindowData.StringInsert(cmd[2])); break;
 			      case 1: list.add(new PanelWindowData.LabelInsert()); break;
@@ -227,7 +244,7 @@ public class Dispatcher extends Thread implements DebugConstants {
 			    }
 			}
 			else
-			if (p.equals("PANELBUTTONEND")&&len==1) {
+			if (p=="PANELBUTTONEND"&&len==1) {
 			    String panel = (String)list.remove(0);
 			    String entry = (String)list.remove(0);
 			    PanelWindowData.addButton(panel, entry, 
@@ -235,79 +252,79 @@ public class Dispatcher extends Thread implements DebugConstants {
 						new PanelWindowData.Insert[list.size()])));
 			}
 			else
-			if (p.equals("MARKPANELENTRY")&&len==5)
+			if (p=="MARKPANELENTRY"&&len==5)
 			    PanelWindowData.markEntry(cmd[1], cmd[2], toBool(cmd[3]), toBool(cmd[4]));
 			else
-			if (p.equals("SELECTPANELENTRY")&&len==3)
+			if (p=="SELECTPANELENTRY"&&len==3)
 			    PanelWindowData.selectEntry(cmd[1], cmd[2]);
 			else
 		    
 		    // proof window commands
-			if (p.equals("OPENPROOF")&&len==3)
+			if (p=="OPENPROOF"&&len==3)
 			    ProofWindow.spawn(cmd[1], toInt(cmd[2]));
 			else
-			if (p.equals("CLOSEPROOF")&&len==2)
+			if (p=="CLOSEPROOF"&&len==2)
 			    ProofWindow.closeproof(toInt(cmd[1]));
 			else
-			if (p.equals("PANEGEOMETRY")&&len==2)
+			if (p=="PANEGEOMETRY"&&len==2)
 			Reply.reply(ProofWindow.getFocussedWindow().getPaneGeometry(toByte(cmd[1])));
 			else
-			if (p.equals("SETPROOFPARAMS")&&len==3)
+			if (p=="SETPROOFPARAMS"&&len==3)
 			    ProofWindow.getFocussedWindow().setProofParams(toByte(cmd[1]), toInt(cmd[2]));
 			else
-			if (p.equals("CLEARPANE")&&len==2)
+			if (p=="CLEARPANE"&&len==2)
 			    ProofWindow.getFocussedWindow().clearPane(toByte(cmd[1]));
 			else
-			if (p.equals("DRAWINPANE")&&len==2)
+			if (p=="DRAWINPANE"&&len==2)
 			    ProofWindow.getFocussedWindow().drawInPane(toByte(cmd[1]));
 			else
 
 		    // disproof
-			if (p.equals("SEQBOX")&&len==4)
+			if (p=="SEQBOX"&&len==4)
 			    ProofWindow.getFocussedWindow().setSequentBox(toInt(cmd[1]), toInt(cmd[2]), toInt(cmd[3]));
 			else
-			if (p.equals("EMPHASISE")&&len==4)
+			if (p=="EMPHASISE"&&len==4)
 			    ProofWindow.getFocussedWindow().emphasise(toInt(cmd[1]), toInt(cmd[2]), toBool(cmd[3]));
 			else
-			if (p.equals("TILESSTART")&&len==1)
+			if (p=="TILESSTART"&&len==1)
 			    list.removeAllElements();
 			else
-			if (p.equals("TILE")&&len==2)
+			if (p=="TILE"&&len==2)
 			    list.add(cmd[1]);
 			else
-			if (p.equals("TILESEND")&&len==1)
+			if (p=="TILESEND"&&len==1)
 			    ProofWindow.getFocussedWindow().setDisproofTiles((String[])list.toArray(new String[list.size()]));
 			else
-			if (p.equals("WORLDSSTART")&&len==1)
+			if (p=="WORLDSSTART"&&len==1)
 			    ProofWindow.getFocussedWindow().worldsStart();
 			else
-			if (p.equals("WORLD")&&len==4)
+			if (p=="WORLD"&&len==4)
 			    ProofWindow.getFocussedWindow().addWorld(toInt(cmd[1]), toInt(cmd[2]), toBool(cmd[3]));
 			else
-			if (p.equals("WORLDLABEL")&&len==5)
+			if (p=="WORLDLABEL"&&len==5)
 			    ProofWindow.getFocussedWindow().addWorldLabel(toInt(cmd[1]), toInt(cmd[2]), toBool(cmd[3]), cmd[4]);
 			else
-			if (p.equals("WORLDCHILD")&&len==5)
+			if (p=="WORLDCHILD"&&len==5)
 			    ProofWindow.getFocussedWindow().addChildWorld(toInt(cmd[1]), toInt(cmd[2]), toInt(cmd[3]), toInt(cmd[4]));
 			else
-			if (p.equals("WORLDSELECT")&&len==3)
+			if (p=="WORLDSELECT"&&len==3)
 			    ProofWindow.getFocussedWindow().selectWorld(toInt(cmd[1]), toInt(cmd[2]), true);
 			else
-			if (p.equals("DISPROOFSELECT")&&len==3)
+			if (p=="DISPROOFSELECT"&&len==3)
 			    ProofWindow.getFocussedWindow().forceDisproofSelect(toInt(cmd[1]), toInt(cmd[2]));
 			else
-			if (p.equals("DISPROOFTEXTSELPOS")&&len==3) {
+			if (p=="DISPROOFTEXTSELPOS"&&len==3) {
 			    list.removeAllElements();
 			    list.add(cmd[1]); list.add(cmd[2]); 
 			}
 			else
-			if (p.equals("DISPROOFTEXTSEL")&&len==2)
+			if (p=="DISPROOFTEXTSEL"&&len==2)
 			    list.add(cmd[1]);
 			else
-			if (p.equals("DISPROOFTEXTSEL")&&len==1)
+			if (p=="DISPROOFTEXTSEL"&&len==1)
 			    list.add("");
 			else
-			if (p.equals("DISPROOFTEXTSELDONE")&&len==1) {
+			if (p=="DISPROOFTEXTSELDONE"&&len==1) {
 			    int posX = toInt((String)list.remove(0));
 			    int posY = toInt((String)list.remove(0));
 			    ProofWindow.getFocussedWindow().forceDisproofTextSelection(
@@ -316,78 +333,78 @@ public class Dispatcher extends Thread implements DebugConstants {
 			else
 			    
 		    // provisos and givens
-			if (p.equals("CLEARPROVISOVIEW")&&len==1)
+			if (p=="CLEARPROVISOVIEW"&&len==1)
 			    ProofWindow.getFocussedWindow().clearProvisoView();
 			else
-			if (p.equals("SHOWPROVISOLINE")&&len==2)
+			if (p=="SHOWPROVISOLINE"&&len==2)
 			    ProofWindow.getFocussedWindow().showProvisoLine(cmd[1]);
 			else
-			if (p.equals("CLEARGIVENS")&&len==1)
+			if (p=="CLEARGIVENS"&&len==1)
 			    list.removeAllElements();
 			else
-			if (p.equals("GIVEN")&&len==3)
+			if (p=="GIVEN"&&len==3)
 			    list.add(new MiscellaneousConstants.IntString(toInt(cmd[1]), cmd[2]));
 			else
-			if (p.equals("SETGIVENS")&&len==1)
+			if (p=="SETGIVENS"&&len==1)
 			    ProofWindow.getFocussedWindow().setGivens(
 				(MiscellaneousConstants.IntString[])
 				    list.toArray(new  MiscellaneousConstants.IntString[list.size()]));
 			else
 
 		    // selections of various kinds
-			if (p.equals("GETPROOFSELECTIONS")&&len==1)
+			if (p=="GETPROOFSELECTIONS"&&len==1)
 			    Reply.reply(ProofWindow.getFocussedWindow().getProofSelections());
 			else
-			if (p.equals("GETPROOFTEXTSELECTIONS")&&len==1)
+			if (p=="GETPROOFTEXTSELECTIONS"&&len==1)
 			    Reply.reply(ProofWindow.getFocussedWindow().getProofTextSelections());
 			else
-			if (p.equals("GETDISPROOFSELECTIONS")&&len==1)
+			if (p=="GETDISPROOFSELECTIONS"&&len==1)
 			    Reply.reply(ProofWindow.getFocussedWindow().getDisproofSelections());
 			else
-			if (p.equals("GETDISPROOFTEXTSELECTIONS")&&len==1)
+			if (p=="GETDISPROOFTEXTSELECTIONS"&&len==1)
 			    Reply.reply(ProofWindow.getFocussedWindow().getDisproofTextSelections());
 			else
-			    if (p.equals("GETGIVENTEXTSELECTIONS")&&len==1)
+			    if (p=="GETGIVENTEXTSELECTIONS"&&len==1)
 			    Reply.reply(ProofWindow.getFocussedWindow().getGivenTextSelections());
 			else
 			
 		    // OPERATOR .. deal with the keyboard in some dialogue boxes
-			if (p.equals("OPERATORSBEGIN")&&len==1)
+			if (p=="OPERATORSBEGIN"&&len==1)
 			    list.removeAllElements();
 			else
-			if (p.equals("OPERATOR")&&len==2)
+			if (p=="OPERATOR"&&len==2)
 			    list.add(cmd[1]);
 			else
-			if (p.equals("OPERATORSEND")&&len==1)
+			if (p=="OPERATORSEND"&&len==1)
 			    TextDialog.setOperators((String[])list.toArray(new String[list.size()]));
 			else
 		    
 		    // miscellaneous
-			if (p.equals("RESETTHEORY")&&len==1) {
+			if (p=="RESETTHEORY"&&len==1) {
 			    JapeMenu.cancelMenus();
 			    PanelWindowData.cancelPanels();
 			    JapeWindow.resetNextPos();
 			}
 			else
-			if (p.equals("EMPTYMENUSANDPANELS")&&len==1) {
+			if (p=="EMPTYMENUSANDPANELS"&&len==1) {
 			    JapeMenu.emptyMenus();
 			    PanelWindowData.emptyPanels();
 			}
 			else
-			if (p.equals("QUIT")&&len==1) {
+			if (p=="QUIT"&&len==1) {
 			    System.exit(0);
 			}
 			else
-			if (p.equals("DONTQUIT")&&len==1)
+			if (p=="DONTQUIT"&&len==1)
 			    Jape.dontQuit();
 			else
-			if (p.equals("SETTEXTSELECTIONMODE")&&len==2)
+			if (p=="SETTEXTSELECTIONMODE"&&len==2)
 			    ProofWindow.setTextSelectionMode(toByte(cmd[1]));
 			else
-			if (p.equals("VERSION")&&len==2)
+			if (p=="VERSION"&&len==2)
 			    AboutBox.setVersion(cmd[1]);
 			else
-			    /*Alert.showErrorAlert*/Logger.log.println("dispatcher doesn't understand ("+len+") "+line);
+			    Alert.showErrorAlert("dispatcher doesn't understand ("+len+") "+line);
 		    } // if (cmd.length!=0)
 		} catch (ProtocolError e) {
 		    Alert.showErrorAlert("protocol error in "+line+":: "+e.getMessage());
