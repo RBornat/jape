9568e6147e2afb9d93866602da64b0087e815e46
Richard Bornat <richard@bornat.me.uk>
2004-05-11 10:22:41 +0000

    cosmetics

--- distrib/camlengine/tactic.mli
+++ distrib/camlengine/tactic.mli
@@ -30,16 +30,17 @@ open Name
 open Termtype
 open Tactictype
 
-val tacname : term -> name
-(* or raise ParseError_ *)
+val tacname : term -> name                  (* or raise ParseError_ *)
+
 val transTactic : term -> tactic
 val explodeForExecute : term -> name * term list
-val tacticstring : tactic -> string
-(* the simple, unvarnished string *)
-val tacticstringwithNLs : tactic -> string
-(* guess what this one does *)
+
+val tacticstring : tactic -> string         (* the simple, unvarnished string *)
+val tacticstringwithNLs : tactic -> string  (* guess what this one does *)
+
 val catelim_tacticstring : tactic -> string list -> string list
 val catelim_tacticstringwithNLs : tactic -> string list -> string list
+
 val remaptactic : (term, term) mapping -> tactic -> tactic
 val isguard : tactic -> bool
 
