5278fc66d42db523af5d62fd8866b578d7c25ae2
Richard Bornat <richard@bornat.me.uk>
2004-05-11 19:31:02 +0000

    cosmetics

--- distrib/camlengine/alert.mli
+++ distrib/camlengine/alert.mli
@@ -57,10 +57,11 @@ val askDangerously : string    -> string * 'a -> string * 'a -> 'a   -> 'a
                    *)
 
 val askChoice : string * string list list -> int option
+
 val defaultseverity       : 'a list -> alertseverity
 val defaultseverity_alert : alertseverity
 val patchalert            : string * alertspec -> unit
 val resetalertpatches     : unit -> unit
 val setComment            : string -> unit (* this demoted to a thing which sets a comment line *)
-val showAlert             : alertseverity -> string -> unit (* this pops up a window *)
+val showAlert             : alertseverity -> string -> unit (* this just pops up a window *)
 
