72eb3def00e0ea8b4179a37427a1eb061b93762d
Richard Bornat <richard@bornat.me.uk>
2004-05-28 15:40:32 +0000

    cosmetics

--- distrib/camlengine/text.mli
+++ distrib/camlengine/text.mli
@@ -30,18 +30,21 @@ open Box
 type font = Displayfont.displayfont
 
 type textalign = FirstLine | MidBlock | LastLine
+
 type syllable = Syllable of (font * string)
               | Gap of int
               | Linebreak of int
               | Block of (textalign * syllable list)
+
 type text = Text of syllable list
+
 type textlayout = Textlayout of (pos * font * string) list
 
 (* Offset positions are relative to a baseline of (0,0), 
  * so a single syllable will have an offset of (0,0).
  *) 
 
-val text_of_string      : font -> string -> text
+val text_of_string   : font -> string -> text
 val measuretext      : (font -> string -> int * int * int) -> textalign -> text 
                     -> textsize * textlayout
 val textlayoutOffset : textlayout -> pos -> textlayout
