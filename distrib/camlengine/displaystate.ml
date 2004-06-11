117a96a708e8919b881cd2dca99b1d98095a407e
Richard Bornat <richard@bornat.me.uk>
2004-06-11 10:32:03 +0000

    added a box and a pos to some types

--- distrib/camlengine/displaystate.ml
+++ distrib/camlengine/displaystate.ml
@@ -25,16 +25,17 @@
 
 *)
 
-type displayclass = Displayclass.displayclass
- and 'a hit        = 'a Hit.hit
- and hitkind       = Hit.hitkind
- and pos           = Box.pos
- and prooftree     = Prooftree.Tree.Fmttree.prooftree
- and path          = Prooftree.Tree.Fmttree.path
+type box          = Box.box
+ and displayclass = Displayclass.displayclass
+ and 'a hit       = 'a Hit.hit
+ and hitkind      = Hit.hitkind
+ and path         = Prooftree.Tree.Fmttree.path
+ and pos          = Box.pos
+ and prooftree    = Prooftree.Tree.Fmttree.prooftree
 
 type displaystaterec =
-      { showProof         : prooftree -> path option -> path option -> displaystate;
-        showFocussedProof : prooftree -> path option -> displaystate;
+      { showProof         : prooftree -> box -> path option -> path option -> displaystate;
+        showFocussedProof : prooftree -> box -> path option -> displaystate;
         refreshProof      : unit -> unit;
         printProof        : out_channel -> prooftree -> path option -> path option -> unit;
         locateHit         : pos -> displayclass option -> hitkind -> path hit option;
--- distrib/camlengine/displaystate.mli
+++ distrib/camlengine/displaystate.mli
@@ -31,18 +31,17 @@ and 'a hit = 'a Hit.hit
 and displayclass = Displayclass.displayclass
 and hitkind = Hit.hitkind
 and pos = Box.pos
+and box = Box.box
 
 type displaystaterec = {
-  showProof : prooftree -> path option -> path option -> displaystate;
-  showFocussedProof : prooftree -> path option -> displaystate;
-  refreshProof : unit -> unit;
-  printProof :
-    out_channel -> prooftree -> path option -> path option -> unit;
-  locateHit : pos -> displayclass option -> hitkind -> path hit option;
-  refineSelection : bool;
-  notifyselect :
-    (pos * displayclass) option -> (pos * displayclass) list -> unit;
-  storedProof : unit -> prooftree option;
+  showProof         : prooftree -> box -> path option -> path option -> displaystate;
+  showFocussedProof : prooftree -> box -> path option -> displaystate;
+  refreshProof      : unit -> unit;
+  printProof        : out_channel -> prooftree -> path option -> path option -> unit;
+  locateHit         : pos -> displayclass option -> hitkind -> path hit option;
+  refineSelection   : bool;
+  notifyselect      : (pos * displayclass) option -> (pos * displayclass) list -> unit;
+  storedProof       : unit -> prooftree option;
 } 
 
 and displaystate = DisplayState of displaystaterec
