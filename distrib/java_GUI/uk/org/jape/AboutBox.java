034868f8d06189e207227f3382823f86fb79a1f3
Richard Bornat <richard@bornat.me.uk>
2005-02-24 18:57:57 +0000

    nicely formatted message, at last

--- distrib/java_GUI/uk/org/jape/AboutBox.java
+++ distrib/java_GUI/uk/org/jape/AboutBox.java
@@ -39,26 +39,25 @@ public class AboutBox {
 	String guiversion = getGUIVersion();
 	Alert.showAlert(Alert.Plain, 
 	    "This is the platform-independent GUI for the Jape proof calculator"+
-	    (guiversion==null ? "" : ", version "+guiversion)+
-	    (engineversion==null ? "" : ", working with Jape proof engine version "+engineversion)+
+	    (guiversion==null ? "" : ",\nversion "+guiversion)+
+	    (engineversion==null ? "" : 
+	     (guiversion==null ? ",\n" : ", ") + "working with Jape proof engine version "+engineversion)+
 	    ".\n\n"+
 	    
 	    "Jape engine and GUI copyright © 2003-5 Richard Bornat & Bernard Sufrin.\n\n"+
 	    
-	    "Jape is free software; you can redistribute it and/or "+
-	    "modify it under the terms of the GNU General Public License "+
-	    "as published by the Free Software Foundation; either version 2 "+
-	    "of the License, or (at your option) any later version.\n\n"+
+	    "Jape is free software; you can redistribute it and/or modify it under\n"+
+	    "the terms of the GNU General Public License as published by the Free\n"+
+	    "Software Foundation; either version 2 of the License, or (at your option)\n"+
+	    "any later version.\n\n"+
 			
-	    "Jape is distributed in the hope that it will be useful, "+
-	    "but WITHOUT ANY WARRANTY; without even the implied warranty of "+
-	    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "+
-	    "GNU General Public License for more details.\n\n"+
+	    "Jape is distributed in the hope that it will be useful, but WITHOUT ANY\n"+
+	    "WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS\n"+
+	    "FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\n"+
 			
-	    "You should have received a copy of the GNU General Public License "+
-	    "with Jape; if not, write to the Free Software "+
-	    "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA, "+
-	    "or look at http://www.gnu.org."
+	    "You should have received a copy of the GNU General Public License with Jape;\n"+
+	    "if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,\n"+
+	    "Boston, MA  02111-1307, USA (or visit http://www.gnu.org)."
 			);
     }
     
