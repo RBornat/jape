d30fae9640cf047103369f94c7ab18fb3f329d58
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2005-04-02 00:00:32 +0000

    Needed slight fix to Unix distribution build

---
+++ distrib/CommonBuildResources/installjape.sh
@@ -0,0 +1,28 @@
+#
+#       Linux/Solaris installation script for jape
+#       To be executed from the directory in which jape is unpacked
+#
+JAPEHOME=$1
+cd $JAPEHOME
+if which java
+then
+   JAVABIN=`which java`
+else
+   echo "Aborting Installation -- please arrange that java is on your PATH during installation"
+   exit 1
+fi
+cat <<ENDSCRIPT>jape
+#!/bin/sh
+        JAPEHOME=$JAPEHOME 
+        export JAPEHOME
+        exec java -jar \$JAPEHOME/Jape.jar -engine \$JAPEHOME/jape_engine \$*
+ENDSCRIPT
+chmod +x jape jape_engine
+echo "Jape is now installed in $JAPEHOME"
+echo "You can make symbolic or other links to the executable $JAPEHOME/jape"
+
+
+
+
+
+
