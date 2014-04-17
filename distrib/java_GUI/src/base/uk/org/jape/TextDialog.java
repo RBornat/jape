4347b6041e828c8f5fd0179104944edd0c78ace9
Bernard Sufrin <Bernard.Sufrin@cs.ox.ac.uk>
2014-04-17 15:53:14 +0000

    Saner treatment of Help menu; tactic-typing entry on edit menu (because debugging jape tactics is tedious enough)

--- distrib/java_GUI/src/base/uk/org/jape/TextDialog.java
+++ distrib/java_GUI/src/base/uk/org/jape/TextDialog.java
@@ -3,8 +3,8 @@
 
     Copyright © 2003-8 Richard Bornat & Bernard Sufrin
      
-	richard@bornat.me.uk
-	sufrin@comlab.ox.ac.uk
+        richard@bornat.me.uk
+        sufrin@comlab.ox.ac.uk
 
     This file is part of the Jape GUI, which is part of Jape.
 
@@ -49,46 +49,57 @@ public class TextDialog {
     private static String[] operators;
 
     public static void setOperators(String[] _operators) {
-	operators=_operators;
+        operators=_operators;
     }
 
     private static String lastCommand = "";
     
     public static void runTextCommandDialog() {
-	String newCommand = runTextDialog("Text Command", lastCommand, 
-					  "Type a command to be sent to the Jape engine");
-	if (newCommand!=null) {
-	    lastCommand = newCommand;
-	    Reply.sendCOMMAND(newCommand);
-	}
+        String newCommand = runTextDialog("Text Command", lastCommand, 
+                                          "Type a command to be sent to the Jape engine");
+        if (newCommand!=null) {
+            lastCommand = newCommand;
+            Reply.sendCOMMAND(newCommand);
+        }
     }
-	
+    
+    private static String lastTacticCommand = "";
+    
+    public static void runTacticCommandDialog() {
+        String newCommand = runTextDialog("Tactic", lastTacticCommand, 
+                                          "Type a 'tactic' to be applied in the proof window");
+        if (newCommand!=null) {
+            lastTacticCommand = newCommand;
+            Reply.sendCOMMAND("APPLY "+newCommand);
+        }
+    }
+        
     private static String lastConjecture = "";
     
     public static void runNewConjectureDialog(String panel) {
-	String newConjecture = runTextDialog("New Conjecture", lastConjecture, 
-					     "Type a new conjecture for the "+panel+" panel");
-	if (newConjecture!=null) {
-	    lastConjecture = newConjecture;
-	    Reply.sendCOMMAND("addnewconjecture "+JapeUtils.enQuote(panel)+" "+newConjecture); /* argument must not be quoted */
-	}
+        String newConjecture = runTextDialog("New Conjecture", lastConjecture, 
+                                             "Type a new conjecture for the "+panel+" panel");
+        if (newConjecture!=null) {
+            lastConjecture = newConjecture;
+            Reply.sendCOMMAND("addnewconjecture "+JapeUtils.enQuote(panel)+" "+newConjecture); /* argument must not be quoted */
+        }
     }
     
     private static String lastUnify = "";
     
     public static void runUnifyDialog(String formula) {
-	String newFormula = runTextDialog("Unify", lastUnify, 
-					  "Type a formula to unify with "+formula);
-	try {
-	    if (newFormula!=null) {
-		lastUnify = newFormula;
-		Reply.reply(newFormula);
-	    }
-	    else
-		Reply.reply(""); 
-	} catch (ProtocolError e) {
-	    Alert.abort("TextDialog.runUnifyDialog can't reply");
-	}   
+        String newFormula = runTextDialog("Unify", lastUnify, 
+                                          "Type a formula to unify with "+formula);
+        try {
+            if (newFormula!=null) {
+                lastUnify = newFormula;
+                Reply.reply(newFormula);
+            }
+            else
+                Reply.reply(""); 
+        } catch (ProtocolError e) {
+            Alert.abort("TextDialog.runUnifyDialog can't reply");
+        }   
     }
 
     public static String runTextDialog(String title, String oldText, String message) {
@@ -122,37 +133,37 @@ public class TextDialog {
                 }
             }
         }
-	OperatorButtonListener buttonPaneListener = new OperatorButtonListener();
-	JPanel display = new JPanel();
-	display.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
-	GridLayout gridLayout = new GridLayout(operators==null?2:3, 1);
-	display.setLayout(gridLayout);
-	display.add(new JLabel(message));
-	display.add(textField);
-	if (operators!=null) {
-	    ButtonPane buttonPane = new ButtonPane();
-	    for (String o: operators) {
-		JButton button = new JButton(o);
-		button.addActionListener(buttonPaneListener);
-		JapeFont.setComponentFont(button, JapeFont.TEXTINPUT);
-		buttonPane.addButton(button);
-		button.setFocusable(false);
-	    }
-	    buttonPane.doLayout();
-	    display.add(buttonPane);
-	}
+        OperatorButtonListener buttonPaneListener = new OperatorButtonListener();
+        JPanel display = new JPanel();
+        display.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        GridLayout gridLayout = new GridLayout(operators==null?2:3, 1);
+        display.setLayout(gridLayout);
+        display.add(new JLabel(message));
+        display.add(textField);
+        if (operators!=null) {
+            ButtonPane buttonPane = new ButtonPane();
+            for (String o: operators) {
+                JButton button = new JButton(o);
+                button.addActionListener(buttonPaneListener);
+                JapeFont.setComponentFont(button, JapeFont.TEXTINPUT);
+                buttonPane.addButton(button);
+                button.setFocusable(false);
+            }
+            buttonPane.doLayout();
+            display.add(buttonPane);
+        }
 
-	// in order to get the text focus in the text field when we start, it's
-	// necessary to run JOptionPane by hand.  Code based on the Sun 1.3.1 docs.
+        // in order to get the text focus in the text field when we start, it's
+        // necessary to run JOptionPane by hand.  Code based on the Sun 1.3.1 docs.
         // And then lots of pragmatic stuff to make it give the focus from the OK 
         // button (which gets it by default) and give it to the text field.
-	String [] options = { "OK", "Cancel" };
+        String [] options = { "OK", "Cancel" };
         final JOptionPane pane = new JOptionPane(display, JOptionPane.PLAIN_MESSAGE, 0,
-					   null, options, options[0]);
-	// pane.set.Xxxx(...); // Configure
-	final JDialog dialog = pane.createDialog(JapeWindow.getTopWindow(), title);
-	dialog.addWindowListener(new WindowAdapter() {
-	    public void windowActivated(WindowEvent e) {
+                                           null, options, options[0]);
+        // pane.set.Xxxx(...); // Configure
+        final JDialog dialog = pane.createDialog(JapeWindow.getTopWindow(), title);
+        dialog.addWindowListener(new WindowAdapter() {
+            public void windowActivated(WindowEvent e) {
                 for (Component c: pane.getComponents()){
                     // pragmatically, it is in a JPanel 
                     if (c instanceof JPanel) {
@@ -174,43 +185,44 @@ public class TextDialog {
             }
         });
 
-	if (Jape.onMacOSX && System.getProperty("java.vm.version").startsWith("1.5.0_")) {
-	    textField.addFocusListener(new FocusAdapter(){
-	        Boolean first = true, atend = false;
-	        public void focusLost(FocusEvent e) {
-	            // for some reason it loses the focus once ...
-	            atend = !first && textField.getSelectionEnd()==textField.getText().length();
-	            // Logger.log.println("focus lost "+first+" "+atend);
-	            first = false;
-	        }
-	        public void focusGained(FocusEvent e) {
-	            if (atend) {
-	                // Logger.log.println("resetting selection to end of "+textField.getText());
-	                textField.setSelectionStart(textField.getSelectionEnd());
-	            }
-	        }
-	    });}
-	
+        if (Jape.onMacOSX && System.getProperty("java.vm.version").startsWith("1.5.0_")) {
+            textField.addFocusListener(new FocusAdapter(){
+                Boolean first = true, atend = false;
+                public void focusLost(FocusEvent e) {
+                    // for some reason it loses the focus once ...
+                    atend = !first && textField.getSelectionEnd()==textField.getText().length();
+                    // Logger.log.println("focus lost "+first+" "+atend);
+                    first = false;
+                }
+                public void focusGained(FocusEvent e) {
+                    if (atend) {
+                        // Logger.log.println("resetting selection to end of "+textField.getText());
+                        textField.setSelectionStart(textField.getSelectionEnd());
+                    }
+                }
+            });}
+        
         JapeMenu.setDialogMenuBar(JapeMenu.TEXTDIALOGWINDOW_BAR|JapeMenu.DIALOGWINDOW_BAR, 
                                   dialog, title);
-	dialog.setVisible(true);
-	
-	Object selectedValue = pane.getValue();
-	/* int reply = JOptionPane.CLOSED_OPTION;
-	if (selectedValue!=null)
-	    for(int counter = 0, maxCounter = options.length;
-		counter < maxCounter; counter++) {
-		if(options[counter].equals(selectedValue)) {
-		    reply = counter; break;
-		}
-	    } */
-		
-	String result = textField.getText();
+        dialog.setVisible(true);
+        
+        Object selectedValue = pane.getValue();
+        /* int reply = JOptionPane.CLOSED_OPTION;
+        if (selectedValue!=null)
+            for(int counter = 0, maxCounter = options.length;
+                counter < maxCounter; counter++) {
+                if(options[counter].equals(selectedValue)) {
+                    reply = counter; break;
+                }
+            } */
+                
+        String result = textField.getText();
 
-	JapeMenu.undoDialogMenuBar();
-	dialog.dispose(); // I hope
-	
-	return selectedValue!=null && options[0].equals(selectedValue) && 
-	         result.length()!=0 ? result : null;
+        JapeMenu.undoDialogMenuBar();
+        dialog.dispose(); // I hope
+        
+        return selectedValue!=null && options[0].equals(selectedValue) && 
+                 result.length()!=0 ? result : null;
     }
 }
+
