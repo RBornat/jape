418984a46913bcf70b15353f4fea4a7894d8c2db
Richard Bornat <richard@bornat.me.uk>
2016-02-02 17:15:11 +0000

    added disproof_howto manual; corrected probs with natural deduction manual

--- distrib/MacOSX/macdistrib.sh
+++ distrib/MacOSX/macdistrib.sh
@@ -13,8 +13,9 @@ make release
 # make the manuals
 
 (cd $MANUALS/roll_your_own; latexmk -pdf roll_your_own; latexmk -c)
-(cd $MANUALS/natural_deduction; latexmk -pdf natural_deduction; latexmk -c)
+(cd $MANUALS/natural_deduction; latexmk -pdf natural_deduction_manual; latexmk -c)
 (cd $MANUALS/forward_reasoning; latexmk -pdf forward_reasoning; latexmk -c)
+(cd $MANUALS/disproof_howto; latexmk -pdf disproof_howto; latexmk -c)
 
 rm -fr $DIR.dir $DIR.dmg
 mkdir $DIR.dir
@@ -28,6 +29,7 @@ cp ../../License.txt $DIR.dir/GPLv2.0.txt
 rsync -rvt  $MANUALS/roll_your_own/roll_your_own.pdf \
             $MANUALS/natural_deduction/natural_deduction_manual.pdf \
             $MANUALS/forward_reasoning/forward_reasoning.pdf \
+            $MANUALS/disproof_howto/disproof_howto.pdf \
             $DIR.dir
 
 sh makedmg $DIR
