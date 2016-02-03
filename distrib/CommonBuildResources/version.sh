121be995c214602d7c6d7a52884614d430fd09ee
Richard Bornat <richard@bornat.me.uk>
2016-02-03 13:57:26 +0000

    version.sh now alerts if you aren't doing a proper tagged release

--- distrib/CommonBuildResources/version.sh
+++ distrib/CommonBuildResources/version.sh
@@ -3,4 +3,5 @@
 
 export MAJOR=`git describe --tags --always --abbrev=0`; \
 export MINOR=`git rev-list $MAJOR.. --count`; \
-echo $MAJOR.$MINOR
+if [ $MINOR = "0" ]; then echo $MAJOR; fi; \
+if [ $MINOR != "0" ]; then echo $MINOR commits after $MAJOR; fi
