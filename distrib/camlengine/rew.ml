98cfa82e879b72a0d82c6af6c6548ba9029c42ac
Richard Bornat <richard@bornat.me.uk>
2020-09-14 15:15:23 +0100

    rewriting SingleDischargeProviso, and some cosmetics

--- distrib/camlengine/rew.ml
+++ distrib/camlengine/rew.ml
@@ -46,31 +46,34 @@ open Termstore
 open Termstring
 open Termtype
 
+(* in 09/2020, this file hadn't been touched since the camlp4 translation from SML-NJ. 
+   It's still hardly been cosmeticised.
+ *)
 let consolereport = Miscellaneous.consolereport
 let idf = fun x -> x
 let string_of_rewinf = Rewinf.string_of_rewinf
 
 let rewritedebug = ref false
 (* the rewrite functions rewrite a term/sequent/proviso, 
-returning Some .. if it rewrites, None if it doesn't.  
-This allows 'conservative' rewrite, which minimises the size of proof states.
+   returning Some .. if it rewrites, None if it doesn't.  
+   This allows 'conservative' rewrite, which minimises the size of proof states.
 
-To speed things up, we have removed the property of rewrites that they 
-return information on variables, etc., present within a rewritten formula.
+   To speed things up, we have removed the property of rewrites that they 
+   return information on variables, etc., present within a rewritten formula.
 *)
 
 (* rewinf is a quadruple of vars, uVIDs, badres, psig; 
-* vars is all names; uVIDs all VIDs from unknowns; 
-* badres all ResUnknown/Nonum resource numbers from enclosed collections;
-* psig is Some int -- the context contains an int which is incremented 
-* when the provisos change significantly (i.e., when they are rewritten or 
-* augmented - and perhaps when they are simplified, but we shall come to that).
-* The vars, uVIDs and badres lists are sorted and don't contain duplicates.
-*)
+ * vars is all names; uVIDs all VIDs from unknowns; 
+ * badres all ResUnknown/Nonum resource numbers from enclosed collections;
+ * psig is Some int -- the context contains an int which is incremented 
+ * when the provisos change significantly (i.e., when they are rewritten or 
+ * augmented - and perhaps when they are simplified, but we shall come to that).
+ * The vars, uVIDs and badres lists are sorted and don't contain duplicates.
+ *)
 
 (* rawinf is the same as rewinf, but it doesn't have the same invariants -- 
-* essentially the lists are unsorted and may contain duplicates
-*)
+ * essentially the lists are unsorted and may contain duplicates
+ *)
 
 type rawinf = term list * vid list * int list * int option
 
@@ -78,125 +81,122 @@ let nullrawinf = [], [], [], None
 
 let mkrawinf raw = raw
 
-let rec raw2rew_ (vars, uVIDs, badres, psig) =
-mkrewinf
-(sortunique earliervar vars, sortunique (<) uVIDs,
- sortunique (<) badres, psig)
-(* these functions designed to be folded *)
+let raw2rew_ (vars, uVIDs, badres, psig) =
+  mkrewinf (sortunique earliervar vars, sortunique (<) uVIDs, sortunique (<) badres, psig)
 
-let rec rawinfTerm n (t, stuff) =
-let rec f (t, (vars, uVIDs, badres, psig as z)) =
-match t with
-  Id _ -> Some (t :: vars, uVIDs, badres, psig)
-| Unknown (_, u, _) -> Some (t :: vars, u :: uVIDs, badres, psig)
-| Subst (_, r, _P, vts) ->
-    let ff = foldterm f in
-    let rec fp ((v, t), z) = ff (ff z v) t in
-    Some (ff (nj_fold fp vts (vars, uVIDs, badres, Some n)) _P)
-| Collection (_, _, es) ->
-    let rec g =
-      function
-        Segvar (_, ms, t), z -> nj_fold (nj_foldterm f) (t :: ms) z
-      | Element (_, ResUnknown i, t), z ->
-          foldterm f (vars, uVIDs, i :: badres, psig) t
-      | Element (_, _, t), z -> foldterm f z t
-    in
-    Some (nj_fold g es z)
-| _ -> None
-in
-foldterm f stuff t
+(* these functions designed to be folded *)
+let rawinfTerm n (t, stuff) =
+  let rec f (t, (vars, uVIDs, badres, psig as z)) =
+    match t with
+    | Id _                  -> Some (t :: vars, uVIDs, badres, psig)
+    | Unknown (_, u, _)     -> Some (t :: vars, u :: uVIDs, badres, psig)
+    | Subst (_, r, _P, vts) -> let ff = foldterm f in
+                               let rec fp ((v, t), z) = ff (ff z v) t in
+                               Some (ff (nj_fold fp vts (vars, uVIDs, badres, Some n)) _P)
+    | Collection (_, _, es) ->
+        let g = function
+                | Segvar (_, ms, t), z -> nj_fold (nj_foldterm f) (t :: ms) z
+                | Element (_, ResUnknown i, t), z ->
+                    foldterm f (vars, uVIDs, i :: badres, psig) t
+                | Element (_, _, t), z -> foldterm f z t
+        in
+        Some (nj_fold g es z)
+    | _                     -> None
+  in
+  foldterm f stuff t
 
 let rec rawinfresnum (r, (vars, uVIDs, badres, psig as raw)) =
-match r with
-ResUnknown i -> vars, uVIDs, i :: badres, psig
-| _ -> raw
+  match r with
+  ResUnknown i -> vars, uVIDs, i :: badres, psig
+  | _          -> raw
 
-let rec rawinfSeq n =
-fun (Seq (st, _Hs, _Gs), stuff) ->
-let _RIT = rawinfTerm n in _RIT (_Hs, _RIT (_Gs, stuff))
+let rawinfSeq n (Seq (st, _Hs, _Gs), stuff) =
+  let _RIT = rawinfTerm n in 
+  _RIT (_Hs, _RIT (_Gs, stuff))
 
-let rec rawinfProviso n (p, stuff) =
+let rawinfProviso n (p, stuff) =
   let _RIT = rawinfTerm n in
+  let _RRE ((r, t), stuff) = rawinfresnum (r, _RIT (t, stuff)) in
   match p with
   | FreshProviso (_, _, _, v)     -> _RIT (v, stuff)
   | NotinProviso (v, p)           -> _RIT (v, _RIT (p, stuff))
   | DistinctProviso vs            -> nj_fold _RIT vs stuff
   | NotoneofProviso (vs, pat, _C) -> nj_fold _RIT vs (_RIT (_C, stuff))
   | UnifiesProviso (p1, p2)       -> _RIT (p1, _RIT (p2, stuff))
+  | SingleDischargeProviso rts    -> nj_fold _RRE rts stuff
+  
+let rawinfElements n (es, stuff) =
+  let rec _RE (e, stuff) =
+    match e with
+    | Segvar (_, ops, v) -> rawinfTerm n (v, nj_fold (rawinfTerm n) ops stuff)
+    | Element (_, r, t)  -> rawinfresnum (r, rawinfTerm n (t, stuff))
+  in
+  nj_fold _RE es stuff
 
-let rec rawinfElements n (es, stuff) =
-let rec _RE (e, stuff) =
-match e with
-  Segvar (_, ops, v) ->
-    rawinfTerm n (v, nj_fold (rawinfTerm n) ops stuff)
-| Element (_, r, t) -> rawinfresnum (r, rawinfTerm n (t, stuff))
-in
-nj_fold _RE es stuff
+let rew_worthwhile subst cxt ri =
+ (subst && (match rewinf_psig ri, getprovisosig cxt with
+            | Some si, ci -> si <> ci
+            | _           -> false
+           ) 
+  || List.exists (bool_of_opt <.> (fun uVID -> (varmap cxt <@> uVID))) (rewinf_uVIDs ri)
+ ) 
+ || List.exists (bool_of_opt <.> (fun i -> (resmap cxt <@> i))) (rewinf_badres ri)
 
-let rec rew_worthwhile subst cxt ri =
-(subst &&
-(match rewinf_psig ri, getprovisosig cxt with
-  Some si, ci -> si <> ci
-| _ -> false) ||
-List.exists (bool_of_opt <.> (fun uVID -> (varmap cxt <@> uVID)))
- (rewinf_uVIDs ri)) ||
-List.exists (bool_of_opt <.> (fun i -> (resmap cxt <@> i)))
-(rewinf_badres ri)
-(* this is almost f &~ (Some o yes), but types get in the way ... *)
+(* this is almost f &~ (_Some <.> yes), but types get in the way ... *)
+(* do they? I think it's f x &~~ (_Some <.> yes) RB 14/09/20 *)
+let rew_ f x yes =
+  match f x with
+  | Some x' -> Some (yes x')
+  | _       -> None
 
-let rec rew_ f x yes =
-match f x with
-Some x' -> Some (yes x')
-| _ -> None
-(* this is Some o anyway f *)
+(* this is _Some <.> anyway f *)
+let rew_yes f t =
+  match f t with
+  | None -> Some t
+  | some -> some
 
-let rec rew_yes f t =
-match f t with
-None -> Some t
-| some -> some
+let rew_2 f x g y yes =
+  (option_rewrite2 f g &~ (_Some <.> yes)) (x, y)
 
-let rec rew_2 f x g y yes =
-(option_rewrite2 f g &~ (_Some <.> yes)) (x, y)
+let rew_Pair _R = option_rewrite2 _R _R
 
-let rec rew_Pair = fun _R -> option_rewrite2 _R _R
 (* there must be a better way ... *)
-
 let rec rew_3 f x g y h z yes =
-(option_rewrite3 f g h &~ (_Some <.> yes)) (x, y, z)
+  (option_rewrite3 f g h &~ (_Some <.> yes)) (x, y, z)
 
-let rec rew_binding outer inner (h, (bs, ss, us), env, pat) =
-rew_3 (option_rewritelist outer) bs (option_rewritelist inner) ss
-(option_rewritelist outer) us
-(fun stuff -> registerBinding (stuff, env, pat))
+let rew_binding outer inner (h, (bs, ss, us), env, pat) =
+  rew_3 (option_rewritelist outer) bs 
+        (option_rewritelist inner) ss
+        (option_rewritelist outer) us
+        (fun stuff -> registerBinding (stuff, env, pat))
 
 let rec rew_Term subst cxt t =
-let rec _S t =
-match t with
-  Id (_, v, _) -> None
-| Unknown (_, v, _) ->
-    begin match (varmap cxt <@> v) with
-      Some t' -> rew_yes _S t'
-    | None -> None
-    end
-| App (h, f, a) -> rew_ (rew_Pair _S) (f, a) registerApp
-| Tup (h, sep, ts) ->
-    rew_ (option_rewritelist _S) ts (fun ts' -> registerTup (sep, ts'))
-| Literal _ -> None
-| Fixapp (h, ss, ts) ->
-    rew_ (option_rewritelist _S) ts
-      (fun ts' -> registerFixapp (ss, ts'))
-| Binding binding -> rew_binding _S _S binding
-| Subst _Pm -> rew_Subst subst cxt _Pm
-| Collection (h, k, es) ->
-    rew_ (rew_elements subst cxt) es
-      (fun es' -> registerCollection (k, es'))
-in
-let res = _S t in
-if !rewritedebug then
-consolereport
-  ["rew_Term "; string_of_bool subst; " cxt "; string_of_termarg t; " => ";
-   string_of_option string_of_term res];
-res
+  let rec _S t =
+    match t with
+    | Id (_, v, _)      -> None
+    | Unknown (_, v, _) -> (match (varmap cxt <@> v) with
+                            | Some t' -> rew_yes _S t'
+                            | None -> None
+                           )
+    | App (h, f, a) -> rew_ (rew_Pair _S) (f, a) registerApp
+    | Tup (h, sep, ts) ->
+        rew_ (option_rewritelist _S) ts (fun ts' -> registerTup (sep, ts'))
+    | Literal _ -> None
+    | Fixapp (h, ss, ts) ->
+        rew_ (option_rewritelist _S) ts
+          (fun ts' -> registerFixapp (ss, ts'))
+    | Binding binding -> rew_binding _S _S binding
+    | Subst _Pm -> rew_Subst subst cxt _Pm
+    | Collection (h, k, es) ->
+        rew_ (rew_elements subst cxt) es
+          (fun es' -> registerCollection (k, es'))
+  in
+  let res = _S t in
+  if !rewritedebug then
+  consolereport
+    ["rew_Term "; string_of_bool subst; " cxt "; string_of_termarg t; " => ";
+     string_of_option string_of_term res];
+  res
 
 and rew_substmap subst cxt vts =
 option_rewritelist (rew_Pair (rew_Term subst cxt)) vts
@@ -209,29 +209,27 @@ match
     (fun (_P, m) ->
        if subst then
          match simp m _P with
-           None -> registerSubst (r, _P, m)
+         | None -> registerSubst (r, _P, m)
          | Some t -> t
        else registerSubst (r, _P, m))
 with
-  None -> if subst then simp m _P else None
+| None -> if subst then simp m _P else None
 | some -> some
 
 and rew_elements subst cxt es =
 let _RT = rew_Term subst cxt in
-let rec def (r, t) =
-rew_ (rew_Term subst cxt) t (fun t -> [registerElement (r, t)])
-in
+let def (r, t) = rew_ _RT t (fun t -> [registerElement (r, t)]) in
 let rec _RE =
 function
-  Segvar (_, [], (Unknown _ as v)) ->
+| Segvar (_, [], (Unknown _ as v)) ->
     rew_ _RT v
       (function
-         Collection (_, _, es) -> es
+       | Collection (_, _, es) -> es
        | _ -> raise (Catastrophe_ ["rew_elements 1"]))
 | Segvar (_, ps, (Unknown _ as v)) as sv ->
     rew_ _RT v
       (function
-         Collection (_, _, []) -> []
+       | Collection (_, _, []) -> []
        | Collection (_, _, es) ->
            (* modeifyelement ps <* es *)
            raise
@@ -247,32 +245,40 @@ function
      * Without this, some replayed proofs generate enormous amounts of
      * space
      *)
-    begin match (resmap cxt <@> i) with
-      Some (r, t) ->
-        rew_yes (rew_elements subst cxt) [registerElement (r, t)]
-    | None -> def (r, t)
-    end
+    (match (resmap cxt <@> i) with
+     | Some (r, t) -> rew_yes (rew_elements subst cxt) [registerElement (r, t)]
+     | None        -> def (r, t)
+    )
 | Element (_, r, t) -> def (r, t)
-| _ -> None
+| _                 -> None
 in
 match es with
 [] -> None
 | e :: es ->
   match _RE e, rew_elements subst cxt es with
-    Some es1, Some es2 -> Some (es1 @ es2)
+  | Some es1, Some es2 -> Some (es1 @ es2)
   | None, Some es -> Some (e :: es)
   | Some es1, None -> Some (es1 @ es)
   | None, None -> None
 
-let rec rew_resnum cxt r =
-match r with
-ResUnknown i ->
-  begin match (resmap cxt <@> i) with
-    Some (r, _) -> rew_yes (rew_resnum cxt) r
-  | None -> None
-  end
-| _ -> None
+and rew_resnum cxt r =
+  match r with
+  | ResUnknown i ->
+      (match (resmap cxt <@> i) with
+       | Some (r, _) -> rew_yes (rew_resnum cxt) r
+       | None        -> None
+      )
+    | _              -> None
 
+and rew_resnum_and_term subst cxt (r, t) = 
+  let def () = rew_ (rew_Term subst cxt) t (fun t -> (r,t)) in
+  match r with
+  | ResUnknown i -> (match (resmap cxt <@> i) with
+                    | Some (r, t) -> rew_resnum_and_term subst cxt (r, t)
+                    | None        -> def ()
+                   )
+  | _            -> def ()
+  
 let rec rew_Seq subst cxt =
   fun (Seq (st, _H, _G)) ->
   rew_2 (rew_Term subst cxt) _H (rew_Term subst cxt) _G
@@ -281,19 +287,18 @@ let rec rew_Seq subst cxt =
 let rec rew_Proviso subst cxt p =
   let _RT = rew_Term subst cxt in
   match p with
-  | FreshProviso (h, g, r, v)     ->
-      rew_ _RT v (fun v -> FreshProviso (h, g, r, v))
-  | NotinProviso vp               -> rew_ (rew_Pair _RT) vp (fun v->NotinProviso v)
-  | DistinctProviso vs            -> rew_ (option_rewritelist _RT) vs (fun v->DistinctProviso v)
-  | NotoneofProviso (vs, pat, _C) ->
-      rew_3 (option_rewritelist _RT) vs _RT pat _RT _C (fun v->NotoneofProviso v)
-  | UnifiesProviso pp             -> rew_ (rew_Pair _RT) pp (fun v->UnifiesProviso v)
-
+  | FreshProviso (h, g, r, v)     -> rew_ _RT v (fun v -> FreshProviso (h, g, r, v))
+  | NotinProviso vp               -> rew_ (rew_Pair _RT) vp _NotinProviso
+  | DistinctProviso vs            -> rew_ (option_rewritelist _RT) vs _DistinctProviso
+  | NotoneofProviso (vs, pat, _C) -> rew_3 (option_rewritelist _RT) vs _RT pat _RT _C _NotoneofProviso
+  | UnifiesProviso pp             -> rew_ (rew_Pair _RT) pp _UnifiesProviso
+  | SingleDischargeProviso rts    -> rew_ (option_rewritelist (rew_resnum_and_term subst cxt)) rts _SingleDischargeProviso
+  
 let rec rew_update a1 a2 a3 a4 =
   match a1, a2, a3, a4 with
-  f, g, Some ri, cxt ->
+| f, g, Some ri, cxt ->
     begin match rewinf_psig ri with
-      Some n ->
+    | Some n ->
         let n' = getprovisosig cxt in
         if n = n' then None else Some (f (rewinf_setpsig ri (Some n')))
     | None -> None
@@ -329,7 +334,7 @@ let rec rew_cxt cxt =
           (provisoresetactual vp)
       in
       match option_rewritelist (rew_visproviso subst cxt) ps with
-        Some ps -> yes (incprovisosig cxt) ps
+      | Some ps -> yes (incprovisosig cxt) ps
       | None -> updatesig ()
     else None
   in
@@ -337,7 +342,7 @@ let rec rew_cxt cxt =
          
   let rec doexterior subst cxt =
     match getexterior cxt with
-      Exterior ((ss, s), inf, fvinf) ->
+    | Exterior ((ss, s), inf, fvinf) ->
         if needsit subst cxt inf then
           let rec yes (ss, s) =
             let raw =
@@ -346,7 +351,7 @@ let rec rew_cxt cxt =
             let inf = raw2rew_ raw in
             let fvinf =
               match rewinf_vars inf, rewinf_uVIDs inf with
-                avs, [] ->
+              | avs, [] ->
                   let (Seq (_, hs, cs)) = s in
                   let hbindings = varbindings hs in
                   let cbindings = varbindings cs in
@@ -377,7 +382,7 @@ let rec rew_cxt cxt =
             rew_2 (option_rewritelist (rew_Seq subst cxt)) ss
               (rew_Seq subst cxt) s idf
           with
-            Some r -> yes r
+          | Some r -> yes r
           | None -> updatesig ()
         else None
     | _ -> None
@@ -387,11 +392,11 @@ let rec rew_cxt cxt =
   let rec doit subst cxt =
     let rec doit2 cxt =
       match doexterior subst cxt with
-        Some cxt -> rew_yes (doit true) cxt
+      | Some cxt -> rew_yes (doit true) cxt
       | None -> if subst then None else doit true cxt
     in
     match doprovisos subst cxt with
-      Some cxt -> rew_yes (if subst then doit true else doit2) cxt
+    | Some cxt -> rew_yes (if subst then doit true else doit2) cxt
     | None -> doit2 cxt
   in
   let r = doit false cxt in
@@ -400,7 +405,7 @@ let rec rew_cxt cxt =
   r
   (*end rew_cxt*)
   
-  let rawinfTerm cxt = rawinfTerm (getprovisosig cxt)
+let rawinfTerm cxt = rawinfTerm (getprovisosig cxt)
 
 let rawinfSeq cxt = rawinfSeq (getprovisosig cxt)
 
