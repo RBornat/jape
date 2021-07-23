ebea359c1dfdfe29257472fe349336d9c3221e73
Richard Bornat <richard@bornat.me.uk>
2021-07-23 16:24:18 +0100

    now creating shortcut

--- distrib/CommonBuildResources/Windows/install.bat
+++ distrib/CommonBuildResources/Windows/install.bat
@@ -4,12 +4,13 @@ SET scriptpath=%scriptpath:~0,-1%
 mkdir %LOCALAPPDATA%\Jape
 rmdir /S /Q %LOCALAPPDATA%\Jape\Jape.app
 powershell Expand-Archive -Force -Path Jape.app.zip -DestinationPath %LOCALAPPDATA%\Jape
-wscript extractToOriginalLocation.vbs /x /e:VBScript
-rmdir %LOCALAPPDATA%\Jape\Jape.app\examples
-@echo off
-echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%HOME%\"Jape examples"\Jape.lnk" >> CreateShortcut.vbs
+echo Dim oWS, sLinkFile, oLink > CreateShortcut.vbs
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> CreateShortcut.vbs
+echo sLinkFile = "Jape.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%LOCALAPPDATA%\Jape\Jape.app\launchstub.bat" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
-cscript CreateShortcut.vbs
+wscript CreateShortcut.vbs
+dir
+wscript extractToOriginalLocation.vbs /x /e:VBScript
+rmdir %LOCALAPPDATA%\Jape\Jape.app\examples
