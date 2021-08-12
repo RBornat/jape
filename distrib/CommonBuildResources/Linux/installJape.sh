d3bcda10bec03d5c3f130b91bc01a0307a2cc530
RBornat <richard@bornat.me.uk>
2021-08-12 14:13:09 +0100

    with a readme and a snapcraft.yaml file

--- distrib/CommonBuildResources/Linux/installJape.sh
+++ distrib/CommonBuildResources/Linux/installJape.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 # 
-# This is the install script for Jape: a bunch of commands that move everything to its
-# correct place.
-# 
-# To install Jape you must 'launch' this script. In some Linuxes (but not usually in Ubuntu)
-# you can double-click the file. If double-clicking shows you this text, then there are two
-# things you can try.
+# If you can see this text, you have opened the installation control file.
 #
-# 1. If you are on Ubuntu, and you'd like to tell it to launch shell scripts that you 
-#    double-click, then paste the following command into a terminal window and press return:
-#
-#       gsettings set org.gnome.nautilus.preferences executable-text-activation 'launch'
-#
-#    and then try double-clicking this file again (it should launch, and install Jape).
-# 
-# 2. Otherwise, open a terminal window, change (cd) to the directory containing this script and
-#    Jape.app, then type or paste the following command and press return:
-#
-#       bash installJape.sh
-#
-#    (it should launch this script and then install Jape).
+# Please close this file, and open READ_ME_HOW_TO_INSTALL_JAPE.
 #
 # -----------------
-# 
-# Once Jape is installed, open the file README_HOW_TO_RUN_JAPE and follow the instructions.
-# 
-# You can close this window now.
-# 
-# -----------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
