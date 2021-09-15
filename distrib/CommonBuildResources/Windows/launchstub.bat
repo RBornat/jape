210325ad0f0453dd5c5fcabe8af96f8aba52943e
RBornat <richard@bornat.me.uk>
2021-09-15 16:12:00 +0100

    long file names in JapeInstall.SED; quoted %mypath% in launchstub.bat

--- distrib/CommonBuildResources/Windows/launchstub.bat
+++ distrib/CommonBuildResources/Windows/launchstub.bat
@@ -3,7 +3,7 @@ SETLOCAL ENABLEEXTENSIONS
 SET mypath=%~dp0
 SET mypath=%mypath:~0,-1%
 echo %mypath%
-cd /D %mypath%
-%mypath%\jre\bin\java -m uk.org.jape/uk.org.jape.Jape
+cd /D "%mypath%"
+"%mypath%"\jre\bin\java -m uk.org.jape/uk.org.jape.Jape
 
 
