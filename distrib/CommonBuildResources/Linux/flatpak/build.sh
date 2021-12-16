d5b0bc60c256c44ce983985c559e16c42e109033
RBornat <richard@bornat.me.uk>
2021-12-16 15:48:14 +0000

    first attempt at flatpak creation

---
+++ distrib/CommonBuildResources/Linux/flatpak/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# install dependencies
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08 
+# org.freedesktop.Sdk.Extension.openjdk11//21.08 
+
+# prepare workspace
+export VERSION=`../../version.sh`
+mkdir tmp
+cp ../LinuxJape_$VERSION.tgz tmp/LinuxJape.tgz
+cd tmp
+tar zxvf LinuxJape.tgz
+cp ../customInstallJape.sh LinuxJape/installJape.sh
+cp ../uk.org.jape.yaml .
+
+flatpak-builder --repo=japerepo build-dir uk.org.jape.yaml --force-clean
+flatpak build-bundle japerepo ../jape.flatpak uk.org.jape
+cd ..
+rm -rf tmp
+
+echo -e "\nresult bundle: $(pwd)/jape.flatpak"
