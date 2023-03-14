9eabb48e5f1798569e00c4c6b20b309bdd06f71c
Richard Bornat <richard@bornat.me.uk>
2023-03-14 16:33:12 +0000

    cosmetics

--- distrib/camlengine/paragraphfuns.mli
+++ distrib/camlengine/paragraphfuns.mli
@@ -36,19 +36,19 @@ open Thing
 
 val interpret :
   (string list -> unit) ->
-    (string list * string * string * int -> bool) -> paraparam list ->
-    proviso list ->
-    japeenv * (name * proofstate * (seq * model) option) list *
-      (name * (string * bool -> unit)) list ->
-    paragraph ->
+  (string list * string * string * int -> bool) -> paraparam list ->
+  proviso list ->
+  japeenv * (name * proofstate * (seq * model) option) list *
+    (name * (string * bool -> unit)) list ->
+  paragraph ->
     japeenv * (name * proofstate * (seq * model) option) list *
       (name * (string * bool -> unit)) list
 val interpretParasFrom :
   (string list -> unit) ->
-    (string list * string * string * int -> bool) ->
-    japeenv * (name * proofstate * (seq * model) option) list *
-      (name * (string * bool -> unit)) list ->
-    string list ->
+  (string list * string * string * int -> bool) ->
+  japeenv * (name * proofstate * (seq * model) option) list *
+    (name * (string * bool -> unit)) list ->
+  string list ->
     japeenv * (name * proofstate * (seq * model) option) list *
       (name * (string * bool -> unit)) list
 val conjecturename : paragraph -> name
